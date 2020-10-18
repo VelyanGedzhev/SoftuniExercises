aa6e8eff7030b58fa78b1b0e2b5feb6227da41b7
Velyan Gedzhev <v.gedzhev@gmail.com>
2020-10-18 18:07:55 +0300

    Add IteratorsAndComparators Lab

---
+++ CSharpAdvancedModule/CSharpAdvanced/IteratorsAndComparatorsLab/IteratorsAndComparators/StartUp.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace IteratorsAndComparators
+{
+    class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            Book bookOne = new Book("Animal Farm", 2003, "George Orwell");
+            Book bookTwo = new Book("The Documents in the Case", 2002, "Dorothy Sayers", "Robert Eustace");
+            Book bookThree = new Book("The Documents in the Case", 1930);
+
+            Library booksLibrary = new Library(bookOne, bookTwo, bookThree);
+
+            foreach (var book in booksLibrary)
+            {
+                Console.WriteLine(book);
+            }
+
+        }
+    }
+}
