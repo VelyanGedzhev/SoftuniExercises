ff74c0f1e78a32a7761c5e974946a79547228d66
Velyan Gedzhev <v.gedzhev@gmail.com>
2020-10-19 20:24:12 +0300

    Add IteratorsAndComparators Exercises

---
+++ CSharpAdvancedModule/CSharpAdvanced/IteratorsAndComparatorsExercise/ComparingObjects/StartUp.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ComparingObjects
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string[] inputInfo = Console.ReadLine()
+                .Split(" ",StringSplitOptions.RemoveEmptyEntries);
+
+            List<Person> people = new List<Person>();
+
+            while (inputInfo[0] != "END")
+            {
+                string name = inputInfo[0];
+                int age = int.Parse(inputInfo[1]);
+                string town = inputInfo[2];
+
+                Person person = new Person(name,age,town);
+                people.Add(person);
+
+                inputInfo = Console.ReadLine()
+                .Split();
+            }
+            int n = int.Parse(Console.ReadLine());
+
+            Person comparedPerson = people[n - 1];
+            int samePersonCount = 0;
+
+            foreach (Person person in people)
+            {
+                if (person.CompareTo(comparedPerson) == 0)
+                {
+                    samePersonCount++;
+                }
+            }
+
+            if (samePersonCount == 1)
+            {
+                Console.WriteLine("No matches");
+            }
+            else
+            {
+                int notSamePersonCount = people.Count - samePersonCount;
+                Console.WriteLine($"{samePersonCount} {notSamePersonCount} {people.Count}");
+            }
+        }
+    }
+}
---
+++ CSharpAdvancedModule/CSharpAdvanced/IteratorsAndComparatorsExercise/EqualityLogic/StartUp.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EqualityLogic
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            HashSet<Person> peopleHasSet = new HashSet<Person>();
+            SortedSet<Person> peopleSortedSet = new SortedSet<Person>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] inputInfo = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                string name = inputInfo[0];
+                int age = int.Parse(inputInfo[1]);
+
+                Person person = new Person(name, age);
+                peopleHasSet.Add(person);
+                peopleSortedSet.Add(person);
+            }
+            Console.WriteLine(peopleHasSet.Count);
+            Console.WriteLine(peopleSortedSet.Count);
+        }
+    }
+}
