2b6d58ebab94270a8d7947ef0df606b9a37b4ad5
Velyan Gedzhev <v.gedzhev@gmail.com>
2020-10-21 11:08:07 +0300

    Add first and second tasks PastExamAugust2020

---
+++ CSharpAdvancedModule/CSharpAdvanced/PastExamsExercise/CSharpAdvancedRetakeExam19August2020/Bee/StartUp.cs
@@ -0,0 +1,105 @@
+﻿using System;
+
+namespace Bee
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int pollinatedFlowers = 0;
+            char[,] territory = new char[n, n];
+            int beeRow = -1;
+            int beeCol = -1;
+
+            for (int row = 0; row < n; row++)
+            {
+                string rowData = Console.ReadLine();
+
+                for (int col = 0; col < n; col++)
+                {
+                    territory[row, col] = rowData[col];
+
+                    if (territory[row, col] == 'B')
+                    {
+                        beeRow = row;
+                        beeCol = col;
+                    }
+                }
+            }
+            string command = Console.ReadLine();
+          
+            while (command != "End")
+            {
+                territory[beeRow, beeCol] = '.';
+                if (command == "left")
+                {
+                    beeCol--;
+                }
+                if (command == "right")
+                {
+                    beeCol++;
+                }
+                if (command == "up")
+                {
+                    beeRow--;
+                }
+                if (command == "down")
+                {
+                    beeRow++;
+                }
+                if (!IsValid(territory, beeRow, beeCol))
+                {
+                    Console.WriteLine($"The bee got lost!");
+
+                    break;
+                }
+                if (territory[beeRow, beeCol] == 'f')
+                {
+                    pollinatedFlowers++;
+                }
+                else if (territory[beeRow, beeCol] == 'O')
+                {
+                    continue;
+                }
+               
+                territory[beeRow, beeCol] = 'B';
+               
+                command = Console.ReadLine();
+            }
+            if (pollinatedFlowers >= 5)
+            {
+                Console.WriteLine($"Great job, the bee managed to pollinate {pollinatedFlowers} flowers!");
+            }
+            else
+            {
+                Console.WriteLine($"The bee couldn't pollinate the flowers, she needed {5 -pollinatedFlowers} flowers more");
+            }
+            PrintTerritory(territory);
+        }
+        public static bool IsValid(char[,] territory, int beeRow, int beeCol)
+        {
+            if (beeRow < 0 || beeCol < 0)
+            {
+                return false;
+            }
+            if (beeRow >= territory.GetLength(0) || beeCol >= territory.GetLength(1))
+            {
+                return false;
+            }
+            return true;
+        }
+        public static void PrintTerritory(char[,] territory)
+        {
+            for (int row = 0; row < territory.GetLength(0); row++)
+            {
+
+                for (int col = 0; col < territory.GetLength(1); col++)
+                {
+                    Console.Write(territory[row, col]);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ CSharpAdvancedModule/CSharpAdvanced/PastExamsExercise/CSharpAdvancedRetakeExam19August2020/FlowerWreaths/StartUp.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FlowerWreaths
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int[] liliesSequence = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int[] rosesSequence = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            Stack<int> lilies = new Stack<int>(liliesSequence);
+            Queue<int> roses = new Queue<int>(rosesSequence);
+
+            int wreathsCounter = 0;
+            int storedSum = 0;
+
+            while (lilies.Count > 0 && roses.Count > 0)
+            {
+                if (lilies.Peek() + roses.Peek() == 15)
+                {
+                    wreathsCounter++;
+                    lilies.Pop();
+                    roses.Dequeue();
+                }
+                else if (lilies.Peek() + roses.Peek() > 15)
+                {
+                    int currentLily = lilies.Pop() - 2;
+                    lilies.Push(currentLily);
+                }
+                else
+                {
+                    storedSum += (lilies.Pop() + roses.Dequeue());
+                }
+            }
+            if (storedSum >= 15)
+            {
+                wreathsCounter += (int)Math.Floor(storedSum / 15.0);
+            }
+            if (wreathsCounter >= 5)
+            {
+                Console.WriteLine($"You made it, you are going to the competition with {wreathsCounter} wreaths!");
+            }
+            else
+            {
+                Console.WriteLine($"You didn't make it, you need {5 - wreathsCounter} wreaths more!");
+            }
+        }
+    }
+}
