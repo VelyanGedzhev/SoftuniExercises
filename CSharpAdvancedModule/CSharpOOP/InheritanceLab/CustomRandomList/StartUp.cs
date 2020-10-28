f680bdbd6bfdb45f3de7828fe12f9b3ecdc1b6e6
Velyan Gedzhev <v.gedzhev@gmail.com>
2020-10-28 13:18:00 +0200

    Add InheritanceLab problems

---
+++ CSharpAdvancedModule/CSharpOOP/InheritanceLab/CustomRandomList/StartUp.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CustomRandomList
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            RandomList list = new RandomList();
+            list.Add("gosho");
+            list.Add("pesho");
+            list.Add("vancho");
+            list.Add("petko");
+            list.Add("mitko");
+
+            Console.WriteLine(list.RandomString());
+        }
+    }
+}
