fe41add24cad6dbd4827ef7f2e03f788ab414bbe
Velyan Gedzhev <v.gedzhev@gmail.com>
2020-11-01 13:20:56 +0200

    Add validation to meet the requirements of the third problem

--- CSharpAdvancedModule/CSharpOOP/EncapsulationLab/PersonsInfo/Person.cs
+++ CSharpAdvancedModule/CSharpOOP/EncapsulationLab/PersonsInfo/Person.cs
@@ -6,6 +6,11 @@ namespace PersonsInfo
 {
     public class Person
     {
+        private string firstName;
+        private string lastName;
+        private int age;
+        private decimal salary;
+
         public Person(string firstName, string lastName, int age, decimal salary)
         {
             FirstName = firstName;
@@ -14,10 +19,60 @@ namespace PersonsInfo
             Salary = salary;
         }
 
-        public string FirstName { get; private set; }
-        public string LastName { get; private set; }
-        public int Age { get; private set; }
-        public decimal Salary { get; private set; }
+        public string FirstName
+        {
+            get => firstName;
+
+            private set
+            {
+                if (value.Length < 3)
+                {
+                    throw new ArgumentException("First name cannot contain fewer than 3 symbols!");
+                }
+                firstName = value;
+            }
+        }
+        public string LastName
+        {
+            get => lastName;
+
+            private set
+            {
+                if (value.Length < 3)
+                {
+                    throw new ArgumentException("Last name cannot contain fewer than 3 symbols!");
+                }
+                lastName = value;
+            }
+        }
+        public int Age 
+        {
+            get => age;
+
+            private set
+            {
+                if (value < 1)
+                {
+                    throw new ArgumentException("Age cannot be zero or a negative integer!");
+                }
+                age = value;
+            }
+        }
+
+        public decimal Salary
+        {
+            get => salary;
+
+            private set
+            {
+                if (value < 460m)
+                {
+                    throw new ArgumentException("Salary cannot be less than 460 leva!");
+                }
+                salary = value;
+            }
+
+        }
         
         public void IncreaseSalary(decimal percentage)
         {
