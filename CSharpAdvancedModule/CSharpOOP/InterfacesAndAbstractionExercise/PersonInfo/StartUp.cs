506d30599c634d1b3ab4ba15f0c33c3673884364
Velyan Gedzhev <v.gedzhev@gmail.com>
2020-11-05 10:59:51 +0200

    Expand the solution to meet the requirements of the second problem

--- CSharpAdvancedModule/CSharpOOP/InterfacesAndAbstractionExercise/PersonInfo/StartUp.cs
+++ CSharpAdvancedModule/CSharpOOP/InterfacesAndAbstractionExercise/PersonInfo/StartUp.cs
@@ -8,10 +8,13 @@ namespace PersonInfo
         {
             string name = Console.ReadLine();
             int age = int.Parse(Console.ReadLine());
-            IPerson person = new Citizen(name, age);
+            string id = Console.ReadLine();
+            string birthdate = Console.ReadLine();
+            IIdentifiable identifiable = new Citizen(name, age, id, birthdate);
+            IBirthable birthable = new Citizen(name, age, id, birthdate);
             
-            Console.WriteLine(person.Name);
-            Console.WriteLine(person.Age);
+            Console.WriteLine(identifiable.Id);
+            Console.WriteLine(birthable.Birthdate);
 
         }
     }
