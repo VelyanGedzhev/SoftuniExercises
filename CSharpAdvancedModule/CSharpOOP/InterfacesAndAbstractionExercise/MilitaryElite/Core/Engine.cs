df0fc12affed63f93c1dd910b39ba61b68a53d1f
Velyan Gedzhev <v.gedzhev@gmail.com>
2020-11-15 17:29:51 +0200

    Add SolidLab problems

--- CSharpAdvancedModule/CSharpOOP/InterfacesAndAbstractionExercise/MilitaryElite/Core/Engine.cs
+++ CSharpAdvancedModule/CSharpOOP/InterfacesAndAbstractionExercise/MilitaryElite/Core/Engine.cs
@@ -3,6 +3,7 @@ using MilitaryElite.Interfaces;
 using MilitaryElite.Models;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace MilitaryElite.Core
@@ -18,26 +19,35 @@ namespace MilitaryElite.Core
                 string[] commandArgs = command
                     .Split();
                 string type = commandArgs[0];
+                int id = int.Parse(commandArgs[1]);
+                string firstName = commandArgs[2];
+                string lastName = commandArgs[3];
+                decimal salary = decimal.Parse(commandArgs[4]);
 
                 ISoldier soldier;
                 ICollection<ISoldier> soldiers = new List<ISoldier>();
+                ICollection<ISoldier> privates = new List<IPrivate>();
 
                 if (type == "Private")
                 {
-                    int id = int.Parse(commandArgs[1]);
-                    string firstName = commandArgs[2];
-                    string lastName = commandArgs[3];
-                    decimal salary = decimal.Parse(commandArgs[4]);
-
                     soldier = new Private(id, firstName, lastName, salary);
                     soldiers.Add(soldier);
                 }
                 else if (type == "LeutenantGeneral")
+                { 
+                    string[] soldiersToAdd = commandArgs.Skip(5).ToArray();
+
+                    foreach (var item in soldiersToAdd)
+                    {
+                        int soldierId = int.Parse(item);
+
+                        privates.Add(soldiers.FirstOrDefault(i => i.Id == soldierId));
+                    }
+                    soldier = new LieutenantGeneral(id, firstName, lastName, salary, privates);
+                }
+                else if (type == "Engineer")
                 {
-                    int id = int.Parse(commandArgs[1]);
-                    string firstName = commandArgs[2];
-                    string lastName = commandArgs[3];
-                    decimal salary = decimal.Parse(commandArgs[4]);
+                    string corp = commandArgs[5];
                 }
             }
         }
