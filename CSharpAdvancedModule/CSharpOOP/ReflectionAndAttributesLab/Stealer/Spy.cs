572de5f7281bce1a812442d2b1a6554b9e76044e
Velyan Gedzhev <v.gedzhev@gmail.com>
2020-11-19 18:10:00 +0200

    Add Stealer problem from ReflectionAndAttributesLab

---
+++ CSharpAdvancedModule/CSharpOOP/ReflectionAndAttributesLab/Stealer/Spy.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace Stealer
+{
+    public class Spy
+    {
+        private const string NAMESPACE = "Stealer";
+        public string StealFieldInfo(string investigateClass, params string[] requestedFields)
+        {
+            Type classType = Type.GetType(NAMESPACE + "." + investigateClass);
+            FieldInfo[] classFields = classType.GetFields(BindingFlags.Public | BindingFlags.Instance | BindingFlags.NonPublic);
+
+
+            StringBuilder sb = new StringBuilder();
+
+            
+            Object classInstance = Activator.CreateInstance(classType, new object[] { });
+
+            sb.AppendLine($"Class under investigation: {NAMESPACE + "." + investigateClass}");
+
+            foreach (FieldInfo field in classFields.Where(f => requestedFields.Contains(f.Name)))
+            {
+                sb.AppendLine($"{field.Name} = {field.GetValue(classInstance)}");
+            }
+
+            return sb.ToString().Trim();
+        }
+    }
+}
