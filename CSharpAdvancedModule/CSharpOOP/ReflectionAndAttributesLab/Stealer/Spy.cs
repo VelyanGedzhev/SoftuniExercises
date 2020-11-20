cb8afaee48a96052301d14918582e2bf4b85f10e
Velyan Gedzhev <v.gedzhev@gmail.com>
2020-11-20 11:33:54 +0200

    Add CollectGettersAndSetters method to meet the requirements of the fourth problem from ReflectionAndAttributesLab

--- CSharpAdvancedModule/CSharpOOP/ReflectionAndAttributesLab/Stealer/Spy.cs
+++ CSharpAdvancedModule/CSharpOOP/ReflectionAndAttributesLab/Stealer/Spy.cs
@@ -76,5 +76,23 @@ namespace Stealer
 
             return sb.ToString().Trim();
         }
+
+        public string CollectGettersAndSetters(string className)
+        {
+            Type classType = Type.GetType(className);
+            MethodInfo[] methods = classType.GetMethods(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
+
+            StringBuilder sb = new StringBuilder();
+
+            foreach (MethodInfo method in methods.Where(n => n.Name.StartsWith("get")))
+            {
+                sb.AppendLine($"{method.Name} will return {method.ReturnType}");
+            }
+            foreach (MethodInfo method in methods.Where(n => n.Name.StartsWith("set")))
+            {
+                sb.AppendLine($"{method.Name} will set field of {method.GetParameters().First().ParameterType}");
+            }
+            return sb.ToString().Trim();
+        }
     }
 }
--- CSharpAdvancedModule/CSharpOOP/ReflectionAndAttributesLab/Stealer/StartUp.cs
+++ CSharpAdvancedModule/CSharpOOP/ReflectionAndAttributesLab/Stealer/StartUp.cs
@@ -8,7 +8,7 @@ namespace Stealer
         {
             Spy spy = new Spy();
             //string result = spy.StealFieldInfo("Hacker", "username", "password");
-            string result = spy.RevealPrivateMethods("Stealer.Hacker");
+            string result = spy.CollectGettersAndSetters("Stealer.Hacker");
             Console.WriteLine(result);
         }
     }
