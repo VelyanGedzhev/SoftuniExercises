dcf5120e8a2855f61e8004bbc24a3f5daad1432d
Velyan Gedzhev <v.gedzhev@gmail.com>
2020-11-20 11:39:45 +0200

    Add the fifth problem from ReflectionAndAttributesLab

---
+++ CSharpAdvancedModule/CSharpOOP/ReflectionAndAttributesLab/AuthorProblem/StartUp.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace AuthorProblem
+{
+    [Author("Pesho")]
+    public class StartUp
+    {
+        [Author("Gosho")]
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
