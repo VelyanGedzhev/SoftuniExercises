902cb56954de9f814dc371f924d945012e66a8e9
Velyan Gedzhev <v.gedzhev@gmail.com>
2020-11-22 15:25:54 +0200

    Add DummyTests problem from UnitTestingLab

--- CSharpAdvancedModule/CSharpOOP/UnitTestingLab/Skeleton.Tests/DummyTests.cs
+++ CSharpAdvancedModule/CSharpOOP/UnitTestingLab/Skeleton.Tests/DummyTests.cs
@@ -1,11 +1,56 @@
 ﻿using NUnit.Framework;
+using System;
 
 [TestFixture]
 public class DummyTests
 {
-    [Test]
-    public void Test1()
+    [TestCase(100, 100, 20, 20)]
+    public void DummyShouldLoseHealthIfAttacked(int health, int exp, int attack, int durability)
     {
+        //Arange
+        Dummy dummy = new Dummy(health, exp);
 
+        //Act
+        dummy.TakeAttack(attack);
+        //Assert
+        var expectedResult = 80;
+        var actualResult = dummy.Health;
+
+        Assert.AreEqual(expectedResult, actualResult);
+
+    }
+
+    [TestCase(0, 100, 20, 20)]
+    public void DeadDummyShouldThrowExceptionIfAttacked(int health, int exp, int attack, int durability)
+    {
+        //Arange
+        Dummy dummy = new Dummy(health, exp);
+        //Act - Assert
+        Assert.Throws<InvalidOperationException>(() =>
+        dummy.TakeAttack(attack));
+        
+    }
+
+    [TestCase(0, 100, 20, 20)]
+    public void DeadDummyShouldGiveExp(int health, int exp, int attack, int durability)
+    {
+        //Arange
+        Dummy dummy = new Dummy(health, exp);
+        //Act - Assert
+        var expectedExp = 100;
+        var actualResult = dummy.GiveExperience();
+
+        Assert.AreEqual(expectedExp, actualResult);
+
+    }
+
+    [TestCase(10, 100, 20, 20)]
+    public void AliveDummyShouldNotGiveExp(int health, int exp, int attack, int durability)
+    {
+        //Arange
+        Dummy dummy = new Dummy(health, exp);
+        //Act - Assert
+        Assert.Throws<InvalidOperationException>(() => 
+        dummy.GiveExperience());
     }
 }
