1bc016dfb401ad86e5e881ee1d1a967bc6a72c98
Velyan Gedzhev <v.gedzhev@gmail.com>
2020-12-01 14:59:57 +0200

    Add Singleton problem form DesignPatternsLab

---
+++ CSharpAdvancedModule/CSharpOOP/DesignPatternsLab/Singleton/Interfaces/ISingletonContainer.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Singleton.Interfaces
+{
+    public interface ISingletonContainer
+    {
+        int GetPopulation(string name);
+    }
+}
---
+++ CSharpAdvancedModule/CSharpOOP/DesignPatternsLab/Singleton/Models/SingletonDataContainer.cs
@@ -0,0 +1,32 @@
+﻿using Singleton.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Singleton.Models
+{
+    public class SingletonDataContainer : ISingletonContainer
+    {
+        private Dictionary<string, int> capitals = new Dictionary<string, int>();
+
+        private SingletonDataContainer()
+        {
+            Console.WriteLine("Initializing singleton object");
+
+            var elements = File.ReadAllLines("capitals.txt");
+
+            for (int i = 0; i < elements.Length; i += 2)
+            {
+                capitals.Add(elements[i], int.Parse(elements[i + 1]));
+            }
+        }
+
+        public int GetPopulation(string name)
+        {
+            return capitals[name];
+            
+        }
+        public static SingletonDataContainer Instance { get; } = new SingletonDataContainer();
+    }
+}
