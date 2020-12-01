c24c2488b87146ac0fafd1e311ffdc52fde31c76
Velyan Gedzhev <v.gedzhev@gmail.com>
2020-12-01 15:16:42 +0200

    Add Facade problem form DesignPatternsLab

---
+++ CSharpAdvancedModule/CSharpOOP/DesignPatternsLab/Facade/StartUp.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Facade
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var car = new CarBuilderFacade()
+                .Info
+                    .WithType("BMW")
+                    .WithColor("Black")
+                    .WithNumberOfDoors(5)
+                .Built
+                    .InCity("Leipzig")
+                    .AtAddress("Some Straße 53")
+                .Build();
+
+            Console.WriteLine(car);
+        }
+    }
+}
