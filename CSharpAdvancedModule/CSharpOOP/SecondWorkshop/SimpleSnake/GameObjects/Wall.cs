964f95fb9e1876e2fd88f972fabfa75e215212cb
Velyan Gedzhev <v.gedzhev@gmail.com>
2020-12-04 13:24:43 +0200

    Add [Wall] class with [SetVerticalLine] and [SetHorizontalLine] methods

---
+++ CSharpAdvancedModule/CSharpOOP/SecondWorkshop/SimpleSnake/GameObjects/Wall.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SimpleSnake.GameObjects
+{
+    public class Wall : Point
+    {
+        private const char WALL_SYMBOL = '\u25A0';
+        public Wall(int leftX, int topY) 
+            : base(leftX, topY)
+        {
+            SetHorizontalLine(0);
+            SetHorizontalLine(TopY - 1);
+
+            SetVerticalLine(0);
+            SetVerticalLine(LeftX - 1);
+        }
+        public void SetHorizontalLine(int topY)
+        {
+            for (int leftX = 0; leftX < LeftX; leftX++)
+            {
+                Draw(leftX, topY, WALL_SYMBOL);
+            }
+        }
+        public void SetVerticalLine(int leftX)
+        {
+            for (int topY = 0; topY < TopY; topY++)
+            {
+                Draw(leftX, topY, WALL_SYMBOL);
+            }
+        }
+    }
+}
--- CSharpAdvancedModule/CSharpOOP/SecondWorkshop/SimpleSnake/StartUp.cs
+++ CSharpAdvancedModule/CSharpOOP/SecondWorkshop/SimpleSnake/StartUp.cs
@@ -1,12 +1,13 @@
 ﻿namespace SimpleSnake
 {
+    using SimpleSnake.GameObjects;
     using Utilities;
 
     public class StartUp
     {
         public static void Main()
         {
-            ConsoleWindow.CustomizeConsole();
+            Wall wall = new Wall(70, 20);
         }
     }
 }
