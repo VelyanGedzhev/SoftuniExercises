0014b9893ea0156a05706d04c4f8a56a801e3d7b
Velyan Gedzhev <v.gedzhev@gmail.com>
2020-12-05 16:28:54 +0200

    Refactor [Start] method in [Map] class

--- CSharpAdvancedModule/CSharpOOP/PastExamsExercise/Exam12April2020/CounterStrike/Models/Maps/Map.cs
+++ CSharpAdvancedModule/CSharpOOP/PastExamsExercise/Exam12April2020/CounterStrike/Models/Maps/Map.cs
@@ -33,7 +33,7 @@ namespace CounterStrike.Models.Maps
                 }
                 if (!IsTeamAlive(terrorists))
                 {
-                    return "Terrorist wins!";
+                    return "CounterTerrorist wins!";
                 }
             }    
         }
