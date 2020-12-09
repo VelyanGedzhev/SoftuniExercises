dfbf4da519a5c2a1889c87edf764d77e8c7c894b
Velyan Gedzhev <v.gedzhev@gmail.com>
2020-12-09 18:12:56 +0200

    Add unit tests for [UnitCar] and [UnitDriver] classes

---
+++ CSharpAdvancedModule/CSharpOOP/PastExamsExercise/RetakeExam22August2020/ExamSkeleton/EasterRaces/Core/Entities/ChampionshipController.cs
@@ -0,0 +1,47 @@
+﻿using EasterRaces.Core.Contracts;
+using EasterRaces.Repositories.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EasterRaces.Core.Entities
+{
+    public class ChampionshipController : IChampionshipController
+    {
+
+        public ChampionshipController()
+        {
+
+        }
+
+        public string AddCarToDriver(string driverName, string carModel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string AddDriverToRace(string raceName, string driverName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string CreateCar(string type, string model, int horsePower)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string CreateDriver(string driverName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string CreateRace(string name, int laps)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string StartRace(string raceName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- CSharpAdvancedModule/CSharpOOP/PastExamsExercise/RetakeExam22August2020/UnitTestsSkeleton/TheRace.Tests/RaceEntryTests.cs
+++ CSharpAdvancedModule/CSharpOOP/PastExamsExercise/RetakeExam22August2020/UnitTestsSkeleton/TheRace.Tests/RaceEntryTests.cs
@@ -1,20 +1,49 @@
 using NUnit.Framework;
+using System;
 using TheRace;
 
 namespace TheRace.Tests
 {
     public class RaceEntryTests
     {
+        private UnitCar car;
+        private UnitDriver driver;
+        private RaceEntry race;
 
         [SetUp]
         public void Setup()
         {
+            car = new UnitCar("Golf", 110, 2000);
+            driver = new UnitDriver("Mikka", car);
+            race = new RaceEntry();
         }
 
         [Test]
-        public void TestOne()
+        [Category("UnitCar")]
+        [TestCase("Golf", 110, 2000)]
+        public void UnitCarConstructorShouldSetPropertiesCorrectly(string model, int horsePower, double cubicCentimeters)
         {
-            Assert.Pass();
+            Assert.AreEqual(model, car.Model);
+            Assert.AreEqual(horsePower, car.HorsePower);
+            Assert.AreEqual(cubicCentimeters, car.CubicCentimeters);
         }
+
+        [Test]
+        [Category("UnitDriver")]
+        public void UnitDriverConstructorShouldSetPropertiesCorrectly()
+        {
+            Assert.AreEqual("Mikka", driver.Name);
+            Assert.AreEqual(car, driver.Car);
+        }
+
+        [Test]
+        [Category("UnitDriver")]
+        public void UnitDriverNameShouldThrowExceptionIfInvalid()
+        {
+            Assert.Throws<ArgumentNullException>(() =>
+            new UnitDriver(null, car));
+        }
+
+        //TODO: Unit tests for RaceEntry
     }
 }
