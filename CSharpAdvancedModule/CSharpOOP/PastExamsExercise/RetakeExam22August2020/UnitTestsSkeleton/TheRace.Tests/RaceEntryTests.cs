5bb83b4f6540c63b12c7fc74cea75b8b35afe99a
Velyan Gedzhev <v.gedzhev@gmail.com>
2020-12-10 12:35:00 +0200

    Add unit tests for [CalculateAverageHorsePower] method

--- CSharpAdvancedModule/CSharpOOP/PastExamsExercise/RetakeExam22August2020/UnitTestsSkeleton/TheRace.Tests/RaceEntryTests.cs
+++ CSharpAdvancedModule/CSharpOOP/PastExamsExercise/RetakeExam22August2020/UnitTestsSkeleton/TheRace.Tests/RaceEntryTests.cs
@@ -7,14 +7,18 @@ namespace TheRace.Tests
     public class RaceEntryTests
     {
         private UnitCar car;
+        private UnitCar anotherCar;
         private UnitDriver driver;
+        private UnitDriver anotherDriver;
         private RaceEntry race;
 
         [SetUp]
         public void Setup()
         {
             car = new UnitCar("Golf", 110, 2000);
+            anotherCar = new UnitCar("Lada", 90, 1800);
             driver = new UnitDriver("Mikka", car);
+            anotherDriver = new UnitDriver("Ivancho", anotherCar);
             race = new RaceEntry();
         }
 
@@ -79,6 +83,26 @@ namespace TheRace.Tests
         {
 
             Assert.AreEqual("Driver Mikka added in race.", race.AddDriver(driver));
+        }
+
+        [Test]
+        [Category("RaceEntry")]
+        public void WhenCalculateAverageHorsePowerIsCalledItShouldThrowExceptionIfDriversCountIsLessThanMinParticipants()
+        {
+            race.AddDriver(driver);
+
+            Assert.Throws<InvalidOperationException>(() =>
+            race.CalculateAverageHorsePower());
+        }
+
+        [Test]
+        [Category("RaceEntry")]
+        public void WhenCalculateAverageHorsePowerIsCalledItShouldReturnCorrectAvgHorsePower()
+        {
+            race.AddDriver(driver);
+            race.AddDriver(anotherDriver);
+
+            Assert.AreEqual(100, race.CalculateAverageHorsePower());
         }
     }
 }
