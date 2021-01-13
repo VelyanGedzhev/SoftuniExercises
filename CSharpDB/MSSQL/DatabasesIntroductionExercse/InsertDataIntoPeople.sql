a60381cedab1c3786939ae3a71694937e8c4b39f
Velyan Gedzhev <v.gedzhev@gmail.com>
2021-01-13 15:34:25 +0200

    Insert data into table [People]

---
+++ CSharpDB/MSSQL/DatabasesIntroductionExercse/InsertDataIntoPeople.sql
@@ -0,0 +1,8 @@
+USE Minions
+
+INSERT INTO People([Name], Picture, Height, Weight, Gender, Birthdate, Biography) VALUES
+('Jurgen', NULL, 1.86, 89, 'm', '1972-02-20', 'Manager'),
+('Bobby', NULL, 1.86, 89, 'm', '1991-02-20', 'Forward'),
+('Abby', NULL, 1.86, 89, 'f', '1992-02-20', 'Medic'),
+('Fan', NULL, 1.86, 89, 'm', '1982-02-20', NULL),
+('SomeGuy', NULL, NULL, NULL, 'm', '1973-02-20', 'Manager')
