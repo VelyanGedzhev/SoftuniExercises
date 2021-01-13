ebdb5c49a9f71a21dcc53be5692f45a6fe4ac3af
Velyan Gedzhev <v.gedzhev@gmail.com>
2021-01-13 15:52:06 +0200

    Refactor table [Users]

--- CSharpDB/MSSQL/DatabasesIntroductionExercse/CreateTableUsers.sql
+++ CSharpDB/MSSQL/DatabasesIntroductionExercse/CreateTableUsers.sql
@@ -2,16 +2,17 @@ USE Minions
 
 CREATE TABLE Users
 (
-	Id BIGINT PRIMARY KEY,
-	Username NVARCHAR(30) NOT NULL,
-	[Password] NVARCHAR(26) NOT NULL,
-	ProfilePicture IMAGE,
+	Id BIGINT PRIMARY KEY IDENTITY,
+	Username VARCHAR(30) NOT NULL,
+	[Password] VARCHAR(26) NOT NULL,
+	ProfilePicture VARCHAR(MAX),
+	LastLoginTime DATETIME,
 	IsDeleted BIT
 )
 
-INSERT INTO Users (Id, Username, [Password], ProfilePicture, IsDeleted) VALUES
-(1, 'One-Two', 'GerrardButler', NULL, 0),
-(2, 'Mumbles', 'IdrisElba', NULL, 0),
-(3, 'Handsome Bob', 'TomHardy', NULL, 0),
-(4, 'Johhny Quid', 'TobyKebbel', NULL, 0),
-(5, 'Lenny Cole', 'TomWilkinson', NULL, 1)
+INSERT INTO Users (Username, [Password], ProfilePicture,LastLoginTime, IsDeleted) VALUES
+('One-Two', 'GerrardButler', 'https://m.media-amazon.com/images/M/MV5BMTQ0NTk5Mzk2OV5BMl5BanBnXkFtZTcwMDE3NTE4MQ@@._V1_.jpg','2021-01-13 15:40:33', 0),
+('Mumbles', 'IdrisElba', NULL,NULL, 0),
+('Handsome Bob', 'TomHardy', NULL,NULL, 0),
+('Johhny Quid', 'TobyKebbel', NULL,NULL, 0),
+('Lenny Cole', 'TomWilkinson', NULL,NULL, 1)
