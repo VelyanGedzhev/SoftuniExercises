0d25bef0c676b458eace2052487e39bf679533c8
Velyan Gedzhev <v.gedzhev@gmail.com>
2021-01-14 15:27:08 +0200

    Populate [SoftUni] database

--- CSharpDB/MSSQL/DatabasesIntroductionExercse/SoftUniDatabase.sql
+++ CSharpDB/MSSQL/DatabasesIntroductionExercse/SoftUniDatabase.sql
@@ -28,12 +28,13 @@ CREATE TABLE Employees
 (
 	Id INT IDENTITY PRIMARY KEY,
 	FirstName VARCHAR(30) NOT NULL,
+	MiddleName VARCHAR(30) NOT NULL,
 	LastName VARCHAR(30) NOT NULL,
 	JobTitle VARCHAR(30) NOT NULL,
 	DepartmentId INT NOT NULL,
-	HireDate DATETIME NOT NULL,
+	HireDate DATE NOT NULL,
 	Salary DECIMAL(15,2),
-	AdressId INT NOT NULL
+	AdressId INT
 )
 
 ALTER TABLE Employees
@@ -52,3 +53,24 @@ TO DISK = 'E:\Programs and drivers\softuni\SoftUni.bak'
 DROP DATABASE SoftUni
 
 RESTORE DATABASE SoftUni FROM DISK = 'E:\Programs and drivers\softuni\SoftUni.bak' 
+
+
+INSERT INTO Towns VALUES
+('Sofia'),
+('Plovdiv'),
+('Varna'),
+('Burgas')
+
+INSERT INTO Departments VALUES
+('Engineering'),
+('Sales'),
+('Marketing'),
+('Software Development'),
+('Quality Assurance')
+
+INSERT INTO Employees VALUES
+('Ivan', 'Ivanov', 'Ivanov', '.NET Developer', 4, '2013/02/01', 3500.00, NULL),
+('Petar', 'Petrov', 'Petrov', 'Senior Engineer', 1, '2004/03/02', 4000.00, NULL),
+('Maria', 'Petrova', 'Ivanova', 'Intern', 5, '2016/08/26', 525.25, NULL),
+('Georgi', 'Teziev', 'Ivanov', 'CEO', 2, '2007/12/09', 3000.00, NULL),
+('Peter', 'Pan', 'Pan', 'Intern', 3, '2016/08/28', 3599.88, NULL)
