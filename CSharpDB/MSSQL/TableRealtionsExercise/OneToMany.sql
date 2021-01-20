4f42a17da8ced52318c2755e4ba69eeaa3a5566c
Velyan Gedzhev <v.gedzhev@gmail.com>
2021-01-20 10:05:18 +0200

    Create One-To-Many relationship

---
+++ CSharpDB/MSSQL/TableRealtionsExercise/OneToMany.sql
@@ -0,0 +1,29 @@
+USE TableRelations
+
+CREATE TABLE Manufacturers
+(
+	ManufacturerID INT PRIMARY KEY IDENTITY,
+	[Name] VARCHAR(20) NOT NULL,
+	EstablishedOn DATETIME
+)
+
+CREATE TABLE Models
+(
+	ModelID INT PRIMARY KEY IDENTITY(101, 1),
+	[Name] VARCHAR(30) NOT NULL,
+	ManufacturerID INT FOREIGN KEY REFERENCES Manufacturers(ManufacturerID)
+)
+
+INSERT INTO Manufacturers ([Name], EstablishedOn) VALUES
+('BMW',	'07/03/1916'),
+('Tesla', '01/01/2003'),
+('Lada', '01/05/1966')
+
+INSERT INTO Models ([Name], ManufacturerID) VALUES 
+('X1',	1),
+('i6',	1),
+('Model S',	2),
+('Model X',	2),
+('Model 3',	2),
+('Nova',	3)
+
