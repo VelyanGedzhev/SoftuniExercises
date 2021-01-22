072167f3b762ae42a00aa4da0f6aaeed04d7c0bf
Velyan Gedzhev <v.gedzhev@gmail.com>
2021-01-22 11:33:55 +0200

    Find all town names that are 5 or 6 symbols long

--- CSharpDB/MSSQL/Built-In Functions/SoftUniDatabaseQueries.sql
+++ CSharpDB/MSSQL/Built-In Functions/SoftUniDatabaseQueries.sql
@@ -28,3 +28,10 @@ SELECT FirstName
 SELECT FirstName, LastName
 	FROM Employees
 	WHERE JobTitle NOT LIKE('%engineer%')
+
+--Problem 5. Find Towns with Name Length
+
+SELECT [Name]
+	FROM Towns
+	WHERE LEN([Name]) = 5 OR LEN([Name]) = 6
+	ORDER BY [Name]
