95b2b4622044d800d9edd013b2eaa40d5ac9a47b
Velyan Gedzhev <v.gedzhev@gmail.com>
2021-01-22 11:25:58 +0200

    Find all which are in given departments and hired in given years

--- CSharpDB/MSSQL/Built-In Functions/SoftUniDatabaseQueries.sql
+++ CSharpDB/MSSQL/Built-In Functions/SoftUniDatabaseQueries.sql
@@ -15,3 +15,10 @@ SELECT FirstName, LastName
 SELECT FirstName, LastName
 	FROM Employees
 	WHERE LastName LIKE('%ei%')
+
+--Problem 3. Find First Names of All Employees
+
+SELECT FirstName
+	FROM Employees
+	WHERE DepartmentID IN (3, 10) 
+	AND (DATEPART(YEAR, HireDate) >= 1995 AND DATEPART(YEAR, HireDate) <= 2005)
