0fc643f2c632fb109e32238bd4e08293ab313927
Velyan Gedzhev <v.gedzhev@gmail.com>
2021-01-22 12:46:47 +0200

    Rank Employees by Salary

--- CSharpDB/MSSQL/Built-In Functions/SoftUniDatabaseQueries.sql
+++ CSharpDB/MSSQL/Built-In Functions/SoftUniDatabaseQueries.sql
@@ -78,3 +78,17 @@ SELECT FirstName, LastName
 SELECT FirstName, LastName
 	FROM Employees
 	WHERE LEN(LastName) = 5
+
+--Problem 10. Rank Employees by Salary
+
+SELECT EmployeeID,
+       FirstName,
+       LastName,
+       Salary,
+       DENSE_RANK()
+               OVER (PARTITION BY Salary
+                   ORDER BY EmployeeID)
+           AS Rank
+FROM Employees
+WHERE Salary BETWEEN 10000 AND 50000
+ORDER BY Salary DESC
