f2c19f8e215ebd4652ff8b02a739e21a9304b62a
Velyan Gedzhev <v.gedzhev@gmail.com>
2021-01-22 13:20:39 +0200

    Combine peak and river names so last letter of the peak is the same as the first letter of the river and mix them

--- CSharpDB/MSSQL/Built-In Functions/GeographyDatabaseQueries.sql
+++ CSharpDB/MSSQL/Built-In Functions/GeographyDatabaseQueries.sql
@@ -6,3 +6,14 @@ SELECT CountryName, IsoCode
 	WHERE CountryName LIKE('%a%a%a%')
 	ORDER BY IsoCode
 
+--Problem 13. Mix of Peak and River Names
+
+SELECT 
+	PeakName, 
+	RiverName,
+	LOWER(CONCAT(PeakName,SUBSTRING(RiverName, 2, LEN(RiverName) -1))) AS mix
+FROM 
+	Peaks,
+	Rivers
+WHERE RIGHT(PeakName, 1) = LEFT(RiverName, 1)
+ORDER BY mix
