706ec9a947df0137e3be924b0fb5ac18e8945271
Velyan Gedzhev <v.gedzhev@gmail.com>
2021-01-26 14:33:02 +0200

    Select employees with salary more than given value

--- CSharpDB/MSSQL/SubqueriesAndJOINsExercise/SubqueriesAndJOINs/SoftuniDBQueries.sql
+++ CSharpDB/MSSQL/SubqueriesAndJOINsExercise/SubqueriesAndJOINs/SoftuniDBQueries.sql
@@ -33,3 +33,15 @@ SELECT
 	WHERE d.Name = 'Sales'
 	ORDER BY e.EmployeeID
 
+--4.Employee Departments
+
+SELECT TOP(5)
+		e.EmployeeID,
+		e.FirstName,
+		e.Salary,
+		d.Name
+	FROM Employees e
+	JOIN Departments d ON e.DepartmentID = d.DepartmentID
+	WHERE Salary > 15000
+	ORDER BY e.DepartmentID
+
