fefd5776690ed439ce53763683210bbc873cdb4a
Velyan Gedzhev <v.gedzhev@gmail.com>
2021-01-26 19:03:32 +0200

    Select employees with a project that is still ongoing

--- CSharpDB/MSSQL/SubqueriesAndJOINsExercise/SubqueriesAndJOINs/SoftuniDBQueries.sql
+++ CSharpDB/MSSQL/SubqueriesAndJOINsExercise/SubqueriesAndJOINs/SoftuniDBQueries.sql
@@ -71,10 +71,12 @@ SELECT
 
 --7.Employees with Project
 
---SELECT 
---		e.EmployeeID,
---		e.FirstName,
---		p.Name as ProjectName
---	FROM Employees e
---	JOIN EmployeesProjects ep ON e.EmployeeID = ep.EmployeeID
---	JOIN Projects p ON ep.ProjectID = p.ProjectID
+SELECT TOP(5)
+		e.EmployeeID,
+		e.FirstName,
+		p.Name as ProjectName
+	FROM Employees e
+	JOIN EmployeesProjects ep ON e.EmployeeID = ep.EmployeeID
+	JOIN Projects p ON ep.ProjectID = p.ProjectID
+	WHERE p.StartDate > '2002-08-13' AND p.EndDate IS NULL
+	ORDER BY e.EmployeeID
