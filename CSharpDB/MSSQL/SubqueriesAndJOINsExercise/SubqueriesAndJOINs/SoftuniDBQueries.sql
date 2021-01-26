1cad32ba1004f406c73ff50ed14e0cd71f0284e5
Velyan Gedzhev <v.gedzhev@gmail.com>
2021-01-26 14:13:15 +0200

    Select first 5 employees sorted by AddressID

---
+++ CSharpDB/MSSQL/SubqueriesAndJOINsExercise/SubqueriesAndJOINs/SoftuniDBQueries.sql
@@ -0,0 +1,12 @@
+USE SoftUni
+
+--1.Employee Address
+
+SELECT TOP(5)
+	EmployeeID, 
+	JobTitle, 
+	e.AddressID,
+	a.AddressText
+FROM Employees e
+JOIN Addresses a ON e.AddressID = a.AddressID
+ORDER BY e.AddressID
