2b3832dc737f376a4bec8c0a9e6c7f97a8faa4ae
Velyan Gedzhev <v.gedzhev@gmail.com>
2021-01-26 14:20:47 +0200

    Select first 50 employees with their addresses and towns

--- CSharpDB/MSSQL/SubqueriesAndJOINsExercise/SubqueriesAndJOINs/SoftuniDBQueries.sql
+++ CSharpDB/MSSQL/SubqueriesAndJOINsExercise/SubqueriesAndJOINs/SoftuniDBQueries.sql
@@ -10,3 +10,13 @@ SELECT TOP(5)
 FROM Employees e
 JOIN Addresses a ON e.AddressID = a.AddressID
 ORDER BY e.AddressID
+
+--2.Addresses with Towns
+
+SELECT TOP(50) e.FirstName, e.LastName, t.Name, a.AddressText
+	FROM Employees e
+	JOIN Addresses a ON e.AddressID = a.AddressID
+	JOIN Towns t ON t.TownID = a.TownID
+	ORDER BY 
+			e.FirstName,
+			e.LastName
