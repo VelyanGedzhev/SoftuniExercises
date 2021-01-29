900b6dbde60bc13ae03ef14e822e6ff2bf3872be
Velyan Gedzhev <v.gedzhev@gmail.com>
2021-01-29 15:05:11 +0200

    Get the longest magic wand in each wizzard group

--- CSharpDB/MSSQL/IndicesАndDataAggregation/GringottsDBQueries.sql
+++ CSharpDB/MSSQL/IndicesАndDataAggregation/GringottsDBQueries.sql
@@ -7,3 +7,8 @@ SELECT COUNT(Id)
 --2. Longest Magic Wand
 SELECT MAX(MagicWandSize) AS LongestMagicWand
 	FROM WizzardDeposits
+
+--3. Longest Magic Wand Per Deposit Groups
+SELECT DepositGroup, MAX(MagicWandSize) AS LongestMagicWand
+	FROM WizzardDeposits
+	GROUP BY DepositGroup
