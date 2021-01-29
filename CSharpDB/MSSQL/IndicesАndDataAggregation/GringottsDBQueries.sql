c368fab34581a747a1dda4c22895c7a4f6f523ba
Velyan Gedzhev <v.gedzhev@gmail.com>
2021-01-29 15:24:45 +0200

    Select all groups, their total deposit sum with wands from given creator and total deposit smaller than given value

--- CSharpDB/MSSQL/IndicesАndDataAggregation/GringottsDBQueries.sql
+++ CSharpDB/MSSQL/IndicesАndDataAggregation/GringottsDBQueries.sql
@@ -33,3 +33,13 @@ SELECT
 	FROM WizzardDeposits
 	WHERE MagicWandCreator = 'Ollivander family'
 	GROUP BY DepositGroup
+
+--7. Deposits Filter
+SELECT 
+		DepositGroup,
+		SUM(DepositAmount) AS TotalSum
+	FROM WizzardDeposits
+	WHERE MagicWandCreator = 'Ollivander family'
+	GROUP BY DepositGroup
+	HAVING SUM(DepositAmount) < 150000
+	ORDER BY TotalSum DESC
