16c143fd753802898f5f4ce1f3425506656832f3
Velyan Gedzhev <v.gedzhev@gmail.com>
2021-01-29 15:13:42 +0200

    Select all groups and their total deposit sums

--- CSharpDB/MSSQL/IndicesАndDataAggregation/GringottsDBQueries.sql
+++ CSharpDB/MSSQL/IndicesАndDataAggregation/GringottsDBQueries.sql
@@ -18,3 +18,10 @@ SELECT TOP(2) DepositGroup
 	FROM WizzardDeposits
 	GROUP BY DepositGroup
 	ORDER BY AVG(MagicWandSize)
+
+--5. Deposits Sum
+SELECT 
+		DepositGroup,
+		SUM(DepositAmount) AS TotalSum
+	FROM WizzardDeposits
+	GROUP BY DepositGroup
