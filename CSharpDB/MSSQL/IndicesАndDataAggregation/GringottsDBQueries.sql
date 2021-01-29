e812443a0bb9c5fde9652780f75c059549e6bd26
Velyan Gedzhev <v.gedzhev@gmail.com>
2021-01-29 15:34:06 +0200

    Select all groups, their wand creator and the minimum deposit charge

--- CSharpDB/MSSQL/IndicesАndDataAggregation/GringottsDBQueries.sql
+++ CSharpDB/MSSQL/IndicesАndDataAggregation/GringottsDBQueries.sql
@@ -43,3 +43,12 @@ SELECT
 	GROUP BY DepositGroup
 	HAVING SUM(DepositAmount) < 150000
 	ORDER BY TotalSum DESC
+
+--8.  Deposit Charge
+SELECT 
+		DepositGroup,
+		MagicWandCreator,
+		MIN(DepositCharge) AS MinDepositCharge
+	FROM WizzardDeposits
+	GROUP BY DepositGroup, MagicWandCreator
+	ORDER BY MagicWandCreator, DepositGroup
