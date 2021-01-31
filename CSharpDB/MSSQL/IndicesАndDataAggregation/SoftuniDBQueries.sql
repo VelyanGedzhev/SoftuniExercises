e585bd35f435107db72137e6138954abd91cc652
Velyan Gedzhev <v.gedzhev@gmail.com>
2021-01-31 14:04:24 +0200

    Create new table, remove employees from given department, increase all salaries and find the avg salary of each department

--- CSharpDB/MSSQL/IndicesАndDataAggregation/SoftuniDBQueries.sql
+++ CSharpDB/MSSQL/IndicesАndDataAggregation/SoftuniDBQueries.sql
@@ -14,3 +14,21 @@ SELECT
 	GROUP BY DepartmentID
 
 --15. Employees Average Salaries
+SELECT *
+	INTO MyNewTable
+	FROM Employees
+	WHERE Salary > 30000
+
+DELETE FROM MyNewTable
+WHERE ManagerID = 42
+
+UPDATE MyNewTable
+SET Salary += 5000
+WHERE DepartmentID = 1
+
+SELECT 
+		DepartmentID,
+		AVG(Salary)
+	FROM MyNewTable
+	GROUP BY DepartmentID
+	
