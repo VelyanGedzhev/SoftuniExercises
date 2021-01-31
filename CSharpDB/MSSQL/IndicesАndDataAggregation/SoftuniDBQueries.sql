c71353e78e00882d17e632d7d157c28c2d4ecbc7
Velyan Gedzhev <v.gedzhev@gmail.com>
2021-01-31 13:47:27 +0200

    Get the lowest salary in given departments for employees hired after given date

--- CSharpDB/MSSQL/IndicesАndDataAggregation/SoftuniDBQueries.sql
+++ CSharpDB/MSSQL/IndicesАndDataAggregation/SoftuniDBQueries.sql
@@ -4,3 +4,11 @@ USE SoftUni
 SELECT DepartmentID, SUM(Salary)
 	FROM Employees 
 	GROUP BY DepartmentID
+
+--14. Employees Minimum Salaries
+SELECT 
+		DepartmentID,
+		MIN(Salary)
+	FROM Employees
+	WHERE DepartmentID IN (2, 5, 7) AND HireDate > '2000/01/01'
+	GROUP BY DepartmentID
