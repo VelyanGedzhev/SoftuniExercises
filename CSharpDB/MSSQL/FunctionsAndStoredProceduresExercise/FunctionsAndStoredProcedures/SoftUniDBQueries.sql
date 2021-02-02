12f14ec5317b3193dfcc72abdb5ba4c7595ee8bc
Velyan Gedzhev <v.gedzhev@gmail.com>
2021-02-02 15:12:18 +0200

    Create procedure that returns all employees with given salary

---
+++ CSharpDB/MSSQL/FunctionsAndStoredProceduresExercise/FunctionsAndStoredProcedures/SoftUniDBQueries.sql
@@ -0,0 +1,14 @@
+USE SoftUni
+
+--1.Employees with Salary Above 35000
+CREATE PROC usp_GetEmployeesSalaryAbove35000 
+AS
+	SELECT 
+			FirstName,
+			LastName
+		FROM Employees
+		WHERE Salary > 35000
+GO
+
+EXEC usp_GetEmployeesSalaryAbove35000
+
