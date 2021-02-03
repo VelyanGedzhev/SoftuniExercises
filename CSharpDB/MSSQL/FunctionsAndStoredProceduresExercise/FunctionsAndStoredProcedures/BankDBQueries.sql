b5defcd37942676a36907d9fbca8dea1a0a731c3
Velyan Gedzhev <v.gedzhev@gmail.com>
2021-02-03 13:39:17 +0200

    Greate proc that calculate future value of given accNumber and given interest value

--- CSharpDB/MSSQL/FunctionsAndStoredProceduresExercise/FunctionsAndStoredProcedures/BankDBQueries.sql
+++ CSharpDB/MSSQL/FunctionsAndStoredProceduresExercise/FunctionsAndStoredProcedures/BankDBQueries.sql
@@ -29,6 +29,17 @@ END
 
 SELECT dbo.ufn_CalculateFutureValue(1000, 0.1, 5)
 --12. Calculating Interest
+CREATE PROC usp_CalculateFutureValueForAccount (@AccountID INT, @InterestRate FLOAT)
+AS
+	SELECT 
+			a.Id,
+			ah.FirstName,
+			ah.LastName,
+			a.Balance,
+			dbo.ufn_CalculateFutureValue(a.Balance, @InterestRate, 5) AS [Balance in 5 years]
+		FROM AccountHolders AS ah
+		JOIN Accounts AS a ON a.AccountHolderId = ah.Id
+		WHERE a.Id = @AccountID
 
-
+EXEC usp_CalculateFutureValueForAccount 1, 0.1
 --13. *Cash in User Games Odd Rows
