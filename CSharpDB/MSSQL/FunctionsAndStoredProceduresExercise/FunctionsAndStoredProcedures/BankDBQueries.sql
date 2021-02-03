ed03663d072241d7c21280501ccd1e8b8d41d63d
Velyan Gedzhev <v.gedzhev@gmail.com>
2021-02-03 13:11:06 +0200

    Create procedure that returns all acc holders with total balance higher that given value

--- CSharpDB/MSSQL/FunctionsAndStoredProceduresExercise/FunctionsAndStoredProcedures/BankDBQueries.sql
+++ CSharpDB/MSSQL/FunctionsAndStoredProceduresExercise/FunctionsAndStoredProcedures/BankDBQueries.sql
@@ -7,9 +7,21 @@ SELECT CONCAT(FirstName, ' ', LastName) AS FullName
 
 EXEC usp_GetHoldersFullName
 --10. People with Balance Higher Than
+CREATE PROC usp_GetHoldersWithBalanceHigherThan (@Value DECIMAL(18,4))
+AS
+	SELECT FirstName, LastName
+		FROM AccountHolders AS ah
+		JOIN Accounts AS a ON a.AccountHolderId = ah.Id
+		GROUP BY FirstName, LastName
+		HAVING SUM(Balance) > @Value
+		ORDER BY FirstName, LastName
+
+EXEC usp_GetHoldersWithBalanceHigherThan 50000
 
 --11. Future Value Function
 
+
 --12. Calculating Interest
+
 
 --13. *Cash in User Games Odd Rows
