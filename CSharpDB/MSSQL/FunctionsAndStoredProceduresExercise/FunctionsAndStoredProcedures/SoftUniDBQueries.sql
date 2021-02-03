01266e3999058ac9576d42f3de39d7aec974948e
Velyan Gedzhev <v.gedzhev@gmail.com>
2021-02-03 12:28:15 +0200

    Create func that returns 0 or 1 if given word is comprised of given set of letters

--- CSharpDB/MSSQL/FunctionsAndStoredProceduresExercise/FunctionsAndStoredProcedures/SoftUniDBQueries.sql
+++ CSharpDB/MSSQL/FunctionsAndStoredProceduresExercise/FunctionsAndStoredProcedures/SoftUniDBQueries.sql
@@ -72,3 +72,22 @@ AS
 		WHERE dbo.ufn_GetSalaryLevel(Salary) = @SalaryLevel
 
 EXEC usp_EmployeesBySalaryLevel 'High'
+
+--07. Define Function
+CREATE FUNCTION ufn_IsWordComprised(@SetOfLetters VARCHAR(MAX), @Word VARCHAR(MAX))
+RETURNS BIT
+BEGIN
+	DECLARE @Count INT = 1
+
+	WHILE (@Count <= LEN(@Word))
+	BEGIN
+		DECLARE @CurrentLetter CHAR(1) = SUBSTRING(@Word, @Count, 1)
+
+		IF(CHARINDEX(@CurrentLetter, @SetOfLetters) = 0)
+			RETURN 0
+		SET @Count += 1
+	END
+	RETURN 1
+END
+
+SELECT dbo.ufn_IsWordComprised ('oistmiahf', 'halves') AS Result
