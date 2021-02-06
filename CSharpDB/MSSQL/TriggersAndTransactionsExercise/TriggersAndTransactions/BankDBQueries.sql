79923d00b9ec990fa4244dcdc7acd948046c8703
Velyan Gedzhev <v.gedzhev@gmail.com>
2021-02-06 12:17:46 +0200

    Add trigger that creates new email when new record is inserted into [Logs]

--- CSharpDB/MSSQL/TriggersAndTransactionsExercise/TriggersAndTransactions/BankDBQueries.sql
+++ CSharpDB/MSSQL/TriggersAndTransactionsExercise/TriggersAndTransactions/BankDBQueries.sql
@@ -9,6 +9,7 @@ CREATE TABLE Logs
 	OldSum MONEY NOT NULL,
 	NewSum MONEY NOT NULL
 )
+GO
 
 CREATE TRIGGER tr_Insert ON Accounts FOR UPDATE
 AS
@@ -19,3 +20,24 @@ AS
 	INSERT INTO Logs (AccountId, OldSum, NewSum) VALUES
 	(@AccountId, @OldSum, @NewSum)
 
+GO
+--2.Create Table Emails
+CREATE TABLE NotificationEmails
+(
+	Id INT PRIMARY KEY IDENTITY,
+	Recipient INT FOREIGN KEY REFERENCES Accounts(Id) NOT NULL,
+	Subject NVARCHAR(100) NOT NULL,
+	Body NVARCHAR(300) NOT NULL
+)
+
+CREATE TRIGGER tr_LogEmail ON Logs FOR INSERT
+AS
+	DECLARE @AccountId INT = (SELECT TOP(1) AccountId FROM inserted)
+	DECLARE @OldSum MONEY = (SELECT TOP(1) OldSum FROM inserted)
+	DECLARE @NewSum MONEY = (SELECT TOP(1) NewSum FROM inserted)
+	DECLARE @EventDate DATETIME = GETDATE()
+
+	INSERT INTO NotificationEmails (Recipient, Subject, Body) VALUES
+	(@AccountId, CONCAT('Balance change for account:', ' ', @AccountId),
+	CONCAT('On', ' ', @EventDate, ' ', 'your balance was changed from', ' ', @OldSum,
+	' ', 'to', ' ', @NewSum))
