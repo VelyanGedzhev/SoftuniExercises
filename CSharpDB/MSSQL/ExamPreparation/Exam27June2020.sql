073b02d7052c1c8a279acc2b25ed1a79e8700d22
Velyan Gedzhev <v.gedzhev@gmail.com>
2021-02-09 16:30:48 +0200

    Add [Section 10. Missing Parts] from Exam27June2020

--- CSharpDB/MSSQL/ExamPreparation/Exam27June2020.sql
+++ CSharpDB/MSSQL/ExamPreparation/Exam27June2020.sql
@@ -152,3 +152,18 @@ SELECT
 	WHERE Status = 'Finished' 
 	GROUP BY j.JobId
 	ORDER BY Total DESC, j.JobId
+
+--10.Missing Parts
+SELECT 
+		p.PartId,
+		p.Description,
+		pn.Quantity AS [Required],
+		p.StockQty AS [In Stock],
+		IIF(o.Delivered = 0, op.Quantity, 0) AS Ordered
+	FROM Parts AS p
+	LEFT JOIN PartsNeeded AS pn ON pn.PartId = p.PartId
+	LEFT JOIN OrderParts AS op ON op.PartId = p.PartId
+	LEFT JOIN Jobs AS j ON j.JobId = pn.JobId
+	LEFT JOIN Orders AS o ON o.JobId = j.JobId
+	WHERE j.Status  != 'Finished' AND p.StockQty + IIF(o.Delivered = 0, op.Quantity, 0) < pn.Quantity
+	ORDER BY p.PartId 
