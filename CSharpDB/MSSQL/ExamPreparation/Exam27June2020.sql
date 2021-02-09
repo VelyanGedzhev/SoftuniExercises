9a63b4e9839e454d106b37a028c7b3f643f2c76a
Velyan Gedzhev <v.gedzhev@gmail.com>
2021-02-09 15:45:50 +0200

    Add [Section 8. Available Mechanics] from Exam27June2020

--- CSharpDB/MSSQL/ExamPreparation/Exam27June2020.sql
+++ CSharpDB/MSSQL/ExamPreparation/Exam27June2020.sql
@@ -129,3 +129,14 @@ SELECT
 	JOIN Jobs AS j ON j.MechanicId = m.MechanicId
 	GROUP BY j.MechanicId, CONCAT(m.FirstName, ' ', m.LastName)
 	ORDER BY j.MechanicId 
+
+--8.Available Mechanics
+SELECT
+		m.FirstName + ' ' +  m.LastName AS Mechanic
+	FROM Mechanics AS m
+	LEFT JOIN Jobs AS j ON j.MechanicId = m.MechanicId
+	WHERE j.JobId IS NULL OR (SELECT COUNT(JobId)
+				FROM Jobs
+				WHERE Status <> 'Finished' AND MechanicId = m.MechanicId
+				GROUP BY MechanicId, Status) IS NULL
+	GROUP BY m.MechanicId, m.FirstName + ' ' +  m.LastName
