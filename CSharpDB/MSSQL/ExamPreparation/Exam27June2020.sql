d05dd4c29cf9f4ce7c8c5a9a5ef09c3502bbfa38
Velyan Gedzhev <v.gedzhev@gmail.com>
2021-02-09 16:08:46 +0200

    Add [Section 9. Past Expenses] from Exam27June2020

--- CSharpDB/MSSQL/ExamPreparation/Exam27June2020.sql
+++ CSharpDB/MSSQL/ExamPreparation/Exam27June2020.sql
@@ -140,3 +140,15 @@ SELECT
 				WHERE Status <> 'Finished' AND MechanicId = m.MechanicId
 				GROUP BY MechanicId, Status) IS NULL
 	GROUP BY m.MechanicId, m.FirstName + ' ' +  m.LastName
+
+--9.Past Expenses
+SELECT 
+		j.JobId,
+		ISNULL(SUM(p.Price * op.Quantity), 0) AS Total
+	FROM Jobs AS j
+	LEFT JOIN Orders AS o ON o.JobId = j.JobId
+	LEFT JOIN OrderParts AS op ON op.OrderId = o.OrderId
+	LEFT JOIN Parts AS p ON p.PartId = op.PartId
+	WHERE Status = 'Finished' 
+	GROUP BY j.JobId
+	ORDER BY Total DESC, j.JobId
