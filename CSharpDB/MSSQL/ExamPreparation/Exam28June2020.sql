293ba8b44b8fbc297b7d541c105c636836d3f138
Velyan Gedzhev <v.gedzhev@gmail.com>
2021-02-11 13:04:30 +0200

    Add [Section 2. DML] from Exam28June2020

--- CSharpDB/MSSQL/ExamPreparation/Exam28June2020.sql
+++ CSharpDB/MSSQL/ExamPreparation/Exam28June2020.sql
@@ -52,4 +52,34 @@ CREATE TABLE TravelCards
 	JourneyId INT FOREIGN KEY REFERENCES Journeys(Id) NOT NULL
 
 )
+--Section 2. DML
+--2. Insert
+INSERT INTO Planets (Name) VALUES
+('Mars'),
+('Earth'),
+('Jupiter'),
+('Saturn')
+
+INSERT INTO Spaceships (Name, Manufacturer, LightSpeedRate) VALUES
+('Golf', 'VW',	3),
+('WakaWaka', 'Wakanda',	4),
+('Falcon9',	'SpaceX',	1),
+('Bed', 'Vidolov',	6)
+
+--3. Update
+UPDATE Spaceships
+	SET LightSpeedRate += 1
+	WHERE Id BETWEEN 8 AND 12
+
+--4. Delete	
+DELETE FROM TravelCards
+	WHERE JourneyId <= 3
+
+DELETE FROM Journeys
+	WHERE Id <= 3
+
+
+
+	
+
 
