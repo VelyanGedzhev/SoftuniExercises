ac339fb07e5f6ce59339707c6fa8609047eb59d4
Velyan Gedzhev <v.gedzhev@gmail.com>
2021-02-16 18:59:07 +0200

    Add [Task 1. Create DB] from ado.net exercise

---
+++ CSharpDB/EF Core/ado.netExercise/ado.netExercise/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.Data.SqlClient;
+
+namespace ado.netExercise
+{
+    public class Program
+    {
+        const string connectionString = "Server=.;Integrated Security=true;Database=master";
+        public static void Main(string[] args)
+        {
+            using (var connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                var createTableArgs = GetCreateTableStatements();
+
+                //create DB
+                string createDB = "CREATE DATABASE MinionsDB";
+
+                using (var command = new SqlCommand(createDB, connection))
+                {
+                    command.ExecuteNonQuery();
+                }
+
+                //create tables
+
+                //insert into tables
+            }
+        }
+
+        private static object GetCreateTableStatements()
+        {
+            var result = new string[]
+            {
+                "CREATE TABLE Countries (Id INT PRIMARY KEY IDENTITY,Name VARCHAR(50))",
+                "CREATE TABLE Towns(Id INT PRIMARY KEY IDENTITY,Name VARCHAR(50), CountryCode INT FOREIGN KEY REFERENCES Countries(Id))",
+                "CREATE TABLE Minions(Id INT PRIMARY KEY IDENTITY,Name VARCHAR(30), Age INT, TownId INT FOREIGN KEY REFERENCES Towns(Id))",
+                "CREATE TABLE EvilnessFactors(Id INT PRIMARY KEY IDENTITY, Name VARCHAR(50))",
+                "CREATE TABLE Villains (Id INT PRIMARY KEY IDENTITY, Name VARCHAR(50), EvilnessFactorId INT FOREIGN KEY REFERENCES EvilnessFactors(Id))",
+                "CREATE TABLE MinionsVillains (MinionId INT FOREIGN KEY REFERENCES Minions(Id),VillainId INT FOREIGN KEY REFERENCES Villains(Id),CONSTRAINT PK_MinionsVillains PRIMARY KEY (MinionId, VillainId))"
+            };
+
+            return result;
+        }
+    }
+}
