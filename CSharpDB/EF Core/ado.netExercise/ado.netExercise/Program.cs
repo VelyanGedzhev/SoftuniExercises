8b6e15d49661f0d1cd4d5ad4c70bf90d7a712a97
Velyan Gedzhev <v.gedzhev@gmail.com>
2021-02-17 14:49:07 +0200

    Add [Task 2. Minion Names] from ado.net exercise

--- CSharpDB/EF Core/ado.netExercise/ado.netExercise/Program.cs
+++ CSharpDB/EF Core/ado.netExercise/ado.netExercise/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using Microsoft.Data.SqlClient;
 
 namespace ado.netExercise
@@ -15,9 +16,59 @@ namespace ado.netExercise
                 //create DB
                 //GetInitialSetup(connection);
 
-                GetVillainNamesAndMinionsCount(connection);
+                //GetVillainNamesAndMinionsCount(connection);
+
+                SqlCommand command = GetMinionNames(connection);
+
             }
         }
+
+        private static SqlCommand GetMinionNames(SqlConnection connection)
+        {
+            int villainId = int.Parse(Console.ReadLine());
+            string villainNameQuery = $"SELECT Name FROM Villains WHERE Id = @Id";
+            var command = new SqlCommand(villainNameQuery, connection);
+            command.Parameters.AddWithValue("@Id", villainId);
+            var result = command.ExecuteScalar();
+
+            string minionsQuery = @"SELECT ROW_NUMBER() OVER (ORDER BY m.Name) as RowNum, m.Name,  m.Age
+                                    FROM MinionsVillains AS mv
+                                    JOIN Minions As m ON mv.MinionId = m.Id
+                                   WHERE mv.VillainId = @Id
+                                ORDER BY m.Name";
+
+            if (result == null)
+            {
+                Console.WriteLine($"No villain with ID {villainId} exists in the database.");
+            }
+            else
+            {
+                Console.WriteLine($"Villain: {result}");
+
+                using (var minionCommand = new SqlCommand(minionsQuery, connection))
+                {
+                    minionCommand.Parameters.AddWithValue("@Id", villainId);
+
+                    using (var reader = minionCommand.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            Console.WriteLine($"{reader[0]}. {reader[1]} {reader[2]}");
+                        }
+                    }
+                }
+            }
+
+            return command;
+        }
+
+        //private static object ExecuteScalar(SqlConnection connection, string villainNameQuery)
+        //{
+        //    using var command = new SqlCommand(villainNameQuery, connection);
+        //    var result = command.ExecuteScalar();
+
+        //    return result;
+        //}
 
         private static void GetVillainNamesAndMinionsCount(SqlConnection connection)
         {
