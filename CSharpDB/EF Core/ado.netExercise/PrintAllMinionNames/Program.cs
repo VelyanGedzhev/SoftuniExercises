c800a6cb76b892cea506d4329e1bed642819e05b
Velyan Gedzhev <v.gedzhev@gmail.com>
2021-02-18 13:23:21 +0200

    Add [Task 7. Print all minions names] from ado.net exercise

---
+++ CSharpDB/EF Core/ado.netExercise/PrintAllMinionNames/Program.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+
+namespace PrintAllMinionNames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string connectionString = "Server=.;Integrated Security=true;Database=MinionsDB";
+
+            using var connection = new SqlConnection(connectionString);
+            connection.Open();
+
+            var minionsQuery = "SELECT Name FROM Minions";
+
+            using var selectCommand = new SqlCommand(minionsQuery, connection);
+
+            using var reader = selectCommand.ExecuteReader();
+            List<string> minions = new List<string>();
+
+            while (reader.Read())
+            {
+                minions.Add(reader[0] as string);
+            }
+
+            int counter = 0;
+
+            for (int i = 0; i < minions.Count / 2; i++)
+            {
+                Console.WriteLine(minions[0 + counter]);
+                Console.WriteLine(minions[minions.Count - 1 - counter]);
+                counter++;
+
+                if (minions.Count % 2 != 0)
+                {
+                    Console.WriteLine(minions[minions.Count / 2]);
+                }
+            }
+        }
+    }
+}
