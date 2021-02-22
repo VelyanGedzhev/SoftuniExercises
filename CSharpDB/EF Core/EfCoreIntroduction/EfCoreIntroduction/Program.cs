72a89ed694fba136df7e1eba2b45d4070576cfef
Velyan Gedzhev <v.gedzhev@gmail.com>
2021-02-22 19:27:28 +0200

    Add EfCoreIntroduction

---
+++ CSharpDB/EF Core/EfCoreIntroduction/EfCoreIntroduction/Program.cs
@@ -0,0 +1,24 @@
+﻿using EfCoreIntroduction.Model;
+using System;
+using System.Linq;
+
+namespace EfCoreIntroduction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var db = new SoftUniContext();
+
+            var topPaidEmployees = db.Employees
+                .OrderByDescending(x => x.Salary)
+                .Take(10)
+                .ToList();
+
+            foreach (var employee in topPaidEmployees)
+            {
+                Console.WriteLine($"{employee.FirstName} {employee.LastName} -- {employee.Salary}");
+            }
+        }
+    }
+}
