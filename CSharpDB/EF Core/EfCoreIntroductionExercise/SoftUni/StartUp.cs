8707fa683a7695a53efeee21654d31297d641e55
Velyan Gedzhev <v.gedzhev@gmail.com>
2021-02-24 13:17:25 +0200

    Add [Task 5. Employees from department] and [Task 6. Add new address and update employee] from EfCoreIntro Exercise

--- CSharpDB/EF Core/EfCoreIntroductionExercise/SoftUni/StartUp.cs
+++ CSharpDB/EF Core/EfCoreIntroductionExercise/SoftUni/StartUp.cs
@@ -1,4 +1,5 @@
 ﻿using SoftUni.Data;
+using SoftUni.Models;
 using System;
 using System.Linq;
 using System.Text;
@@ -14,8 +15,14 @@ namespace SoftUni
             //var employees = GetEmployeesFullInformation(db);
             //Console.WriteLine(employees);
 
-            var employees = GetEmployeesWithSalaryOver50000(db);
-            Console.WriteLine(employees);
+            //var employees = GetEmployeesWithSalaryOver50000(db);
+            //Console.WriteLine(employees);
+
+            //var employees = GetEmployeesFromResearchAndDevelopment(db);
+            //Console.WriteLine(employees);
+
+            var addresses = AddNewAddressToEmployee(db);
+            Console.WriteLine(addresses);
 
         }
 
@@ -61,6 +68,83 @@ namespace SoftUni
             foreach (var employee in employees)
             {
                 sb.AppendLine($"{employee.FirstName} - {employee.Salary:f2}");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+
+        public static string GetEmployeesFromResearchAndDevelopment(SoftUniContext context)
+        {
+            var employees = context.Employees
+                .Select(x => new
+                {
+                    x.FirstName,
+                    x.LastName,
+                    x.Department,
+                    x.Salary
+                })
+                .Where(x => x.Department.Name == "Research and Development")
+                .OrderBy(x => x.Salary)
+                .ThenByDescending(x => x.FirstName)
+                .ToList();
+
+
+            StringBuilder sb = new StringBuilder();
+
+            foreach (var employee in employees)
+            {
+                sb.AppendLine($"{employee.FirstName} {employee.LastName} from {employee.Department.Name} - ${employee.Salary:f2}");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+
+        public static string AddNewAddressToEmployee(SoftUniContext context)
+        {
+            //First solution
+            /*var address = new Address
+            {
+                AddressText = "Vitoshka 15",
+                TownId = 4,
+            };
+
+            context.Addresses.Add(address);
+            context.SaveChanges();
+
+            var requiredEmployee = context.Employees
+                .FirstOrDefault(x => x.LastName == "Nakov");
+
+            requiredEmployee.AddressId = address.AddressId;
+            context.SaveChanges();*/
+
+            //Second solution
+
+            var requiredEmployee = context.Employees
+                .FirstOrDefault(x => x.LastName == "Nakov");
+
+            requiredEmployee.Address = new Address
+            {
+                AddressText = "Vitoshka 15",
+                TownId = 4,
+            };
+
+            context.SaveChanges();
+
+            var addresses = context.Employees
+                .Select(x => new
+                {
+                    x.AddressId,
+                    x.Address.AddressText,
+                })
+                .OrderByDescending(x => x.AddressId)
+                .Take(10)
+                .ToList();
+
+            StringBuilder sb = new StringBuilder();
+
+            foreach (var address in addresses)
+            {
+                sb.AppendLine(address.AddressText);
             }
 
             return sb.ToString().TrimEnd();
