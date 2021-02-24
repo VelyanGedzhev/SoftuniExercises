a62d8b079b5698f312893a367b8c9ce5c23d83bd
Velyan Gedzhev <v.gedzhev@gmail.com>
2021-02-24 12:37:42 +0200

    Add [Task 2. Employees full information] from EfCoreIntro Exercise

--- CSharpDB/EF Core/EfCoreIntroductionExercise/SoftUni/StartUp.cs
+++ CSharpDB/EF Core/EfCoreIntroductionExercise/SoftUni/StartUp.cs
@@ -1,6 +1,7 @@
 ﻿using SoftUni.Data;
 using System;
 using System.Linq;
+using System.Text;
 
 namespace SoftUni
 {
@@ -8,7 +9,35 @@ namespace SoftUni
     {
         public static void Main(string[] args)
         {
-            
+            var db = new SoftUniContext();
+            var employeesFullInfo = GetEmployeesFullInformation(db);
+
+            Console.WriteLine(employeesFullInfo);
+        }
+
+        public static string GetEmployeesFullInformation(SoftUniContext context)
+        {
+            var employees = context.Employees
+                .Select(x => new
+                {
+                    x.EmployeeId,
+                    x.FirstName,
+                    x.MiddleName,
+                    x.LastName,
+                    x.JobTitle,
+                    x.Salary,
+                })
+                .OrderBy(x => x.EmployeeId)
+                .ToList();
+
+            StringBuilder sb = new StringBuilder();
+
+            foreach (var employee in employees)
+            {
+                sb.AppendLine($"{employee.FirstName} {employee.LastName} {employee.MiddleName} {employee.JobTitle} {employee.Salary:f2}");
+            }
+
+            return sb.ToString().TrimEnd();
         }
     }
 }
