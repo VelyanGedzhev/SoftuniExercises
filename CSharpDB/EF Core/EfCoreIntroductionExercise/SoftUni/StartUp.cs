87cc7dd6121b2e8800005954f12d3611a1f08779
Velyan Gedzhev <v.gedzhev@gmail.com>
2021-02-24 12:31:25 +0200

    Add [Task 1. DatabaseFirst] from EfCoreIntro Exercise

---
+++ CSharpDB/EF Core/EfCoreIntroductionExercise/SoftUni/StartUp.cs
@@ -0,0 +1,14 @@
+﻿using SoftUni.Data;
+using System;
+using System.Linq;
+
+namespace SoftUni
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            
+        }
+    }
+}
