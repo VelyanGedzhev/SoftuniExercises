145b14722f4fbcf776f99523a5a00d014dbfc272
Velyan Gedzhev <v.gedzhev@gmail.com>
2021-02-25 20:37:56 +0200

    Add [Attributes] demo from Entity Relations

---
+++ CSharpDB/EF Core/EntityRelations/AttributesDemo/Models/ApplicationDbContext.cs
@@ -0,0 +1,34 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+
+
+namespace AttributesDemo.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext()
+        {
+        }
+
+        public ApplicationDbContext(DbContextOptions options)
+            :base()
+        {
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+
+        public DbSet<Department> Departments { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Server=.;Database=EntityRelationsAttributes;Integrated Security=true;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ CSharpDB/EF Core/EntityRelations/AttributesDemo/Models/Employee.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace AttributesDemo.Models
+{
+    //[Table("People", Schema = "company")]
+    [Index("FirstName", "Salary")]
+    public class Employee
+    {
+        [Key]
+        public int EID { get; set; }
+
+        [Required]
+        [MaxLength(20)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [MaxLength(30)]
+        public string LastName { get; set; }
+
+        [NotMapped]
+        public string FullName => $"{FirstName} {LastName}";
+
+        [Column("Started On", TypeName = "date")]
+        public DateTime? StartWorkDate { get; set; }
+
+        public decimal Salary { get; set; }
+
+        //Optional
+        public int DepId { get; set; }
+
+        //Required
+        [ForeignKey(nameof(DepId))]
+        [InverseProperty("Employees")] //optional
+        public Department Department { get; set; }
+    }
+}
