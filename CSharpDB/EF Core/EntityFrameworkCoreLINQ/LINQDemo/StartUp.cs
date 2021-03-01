b42809bfe3188bba2a38d450cbff285b01f2453e
Velyan Gedzhev <v.gedzhev@gmail.com>
2021-03-01 19:50:57 +0200

    Add [GetSongsWithArtists] from EntityFrameworkCoreLINQ

---
+++ CSharpDB/EF Core/EntityFrameworkCoreLINQ/LINQDemo/StartUp.cs
@@ -0,0 +1,33 @@
+﻿using LINQDemo.Models;
+using System;
+using System.Linq;
+
+namespace LINQDemo
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            var db = new MusicXContext();
+            GetSongsWithArtists(db);
+        }
+
+        private static void GetSongsWithArtists(MusicXContext db)
+        {
+            var songs = db.Songs
+                            .Where(x => x.Source.Name == "User")
+                            .Select(x => new
+                            {
+                                x.Name,
+                                Source = x.Source.Name,
+                                Artists = string.Join(", ", x.SongArtists.Select(a => a.Artist.Name))
+                            })
+                            .ToList();
+
+            foreach (var song in songs)
+            {
+                Console.WriteLine(song.Artists + " - " + song.Name);
+            }
+        }
+    }
+}
