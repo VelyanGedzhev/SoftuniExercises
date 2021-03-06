0c2580db5bd4f034b360a4cd6192b0bd13f04693
Velyan Gedzhev <v.gedzhev@gmail.com>
2021-03-06 11:01:24 +0200

    Add [Task 1. Age Restriction] from EfCoreAdvancedQueryingExercise

--- CSharpDB/EF Core/EfCoreAdvancedQueryingExercise/BookShop/StartUp.cs
+++ CSharpDB/EF Core/EfCoreAdvancedQueryingExercise/BookShop/StartUp.cs
@@ -1,7 +1,11 @@
 ﻿namespace BookShop
 {
+    using BookShop.Models.Enums;
     using Data;
     using Initializer;
+    using System;
+    using System.Linq;
+    using System.Text;
 
     public class StartUp
     {
@@ -9,6 +13,21 @@
         {
             using var db = new BookShopContext();
             DbInitializer.ResetDatabase(db);
+
+            var command = Console.ReadLine();
+            Console.WriteLine(GetBooksByAgeRestriction(db, command));
+        }
+
+        public static string GetBooksByAgeRestriction(BookShopContext context, string command)
+        {
+            var ageRestriction = Enum.Parse<AgeRestriction>(command, true);
+            var books = context.Books
+                .Where(x => x.AgeRestriction == ageRestriction)
+                .Select(x => x.Title)
+                .OrderBy(x => x)
+                .ToList();
+
+            return string.Join(Environment.NewLine, books);
         }
     }
 }
