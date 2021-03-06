a248e61ab5ec03c9b1f17841bd18fc758a06cb13
Velyan Gedzhev <v.gedzhev@gmail.com>
2021-03-06 11:15:26 +0200

    Add [Task 2.Golden Books] from EfCoreAdvancedQueryingExercise

--- CSharpDB/EF Core/EfCoreAdvancedQueryingExercise/BookShop/StartUp.cs
+++ CSharpDB/EF Core/EfCoreAdvancedQueryingExercise/BookShop/StartUp.cs
@@ -14,8 +14,10 @@
             using var db = new BookShopContext();
             DbInitializer.ResetDatabase(db);
 
-            var command = Console.ReadLine();
-            Console.WriteLine(GetBooksByAgeRestriction(db, command));
+            //var command = Console.ReadLine();
+            //Console.WriteLine(GetBooksByAgeRestriction(db, command));
+            Console.WriteLine(GetGoldenBooks(db));
+
         }
 
         public static string GetBooksByAgeRestriction(BookShopContext context, string command)
@@ -28,6 +30,24 @@
                 .ToList();
 
             return string.Join(Environment.NewLine, books);
+        }
+
+        public static string GetGoldenBooks(BookShopContext context)
+        {
+            var books = context.Books
+                .Where(x => x.EditionType == EditionType.Gold && x.Copies < 5000)
+                .Select(x => new 
+                { 
+                    Title = x.Title,
+                    Id = x.BookId,
+                })
+                .OrderBy(x => x.Id)
+                .ToList();
+
+            var result = string.Join(Environment.NewLine, 
+                books.Select(x => x.Title));
+
+            return result;
         }
     }
 }
