12c19b07a311edfa5b8cfc8d2f42f8563c253d3e
Velyan Gedzhev <v.gedzhev@gmail.com>
2021-03-08 18:33:18 +0200

    Add skeleton for AutoMappingObjects

---
+++ CSharpDB/EF Core/AutoMappingObjects/AutoMappingDemo/StartUp.cs
@@ -0,0 +1,39 @@
+﻿using AutoMappingDemo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AutoMappingDemo
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            var songs = GetSongs("Ni");
+
+            Console.WriteLine(string.Join(Environment.NewLine, songs.Select(x => x.Name)));
+        }
+
+        public static IEnumerable<SongInfoDto> GetSongs(string startsWith)
+        {
+            var db = new MusicXContext();
+
+            var songs = db.Songs
+                .Where(x => x.Name.StartsWith(startsWith))
+                .Select(x => new SongInfoDto
+                {
+                    Name = x.Name,
+                    SourceName = x.Source.Name,
+                }).ToList();
+
+            return songs;
+        }
+    }
+
+    public class SongInfoDto
+    {
+        public string Name { get; set; }
+
+        public string SourceName { get; set; }
+    }
+}
