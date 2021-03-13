daf475b533baa42e591c43d0b747a5e9cf6889d2
Velyan Gedzhev <v.gedzhev@gmail.com>
2021-03-13 14:31:18 +0200

    Add [Task 1.Import Users] from JSONProcessingExercise

--- CSharpDB/EF Core/JSONProcessingExercise/ProductShop/ProductShop/StartUp.cs
+++ CSharpDB/EF Core/JSONProcessingExercise/ProductShop/ProductShop/StartUp.cs
@@ -2,17 +2,53 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using AutoMapper;
 using Newtonsoft.Json;
 using ProductShop.Data;
+using ProductShop.DataTransferObjects;
 using ProductShop.Models;
 
 namespace ProductShop
 {
     public class StartUp
     {
+        static IMapper mapper;
         public static void Main(string[] args)
         {
+            var db = new ProductShopContext();
+            db.Database.EnsureDeleted();
+            db.Database.EnsureCreated();
+            
 
+            var inputJson = File.ReadAllText("../../../Datasets/users.json");
+            var result = ImportUsers(db, inputJson);
+            Console.WriteLine(result);
+        }
+
+
+        public static string ImportUsers(ProductShopContext context, string inputJson)
+        {
+            InitializeAutomapper();
+
+            var dtoUsers = JsonConvert.DeserializeObject<IEnumerable<UserInputModel>>(inputJson);
+
+            var users = mapper.Map<IEnumerable<User>>(dtoUsers);
+
+            context.Users.AddRange(users);
+            context.SaveChanges();
+
+            return $"Successfully imported {users.Count()}";
+        }
+
+
+        private static void InitializeAutomapper()
+        {
+            var config = new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile<ProductShopProfile>();
+            });
+
+            mapper = config.CreateMapper();
         }
     }
 }
