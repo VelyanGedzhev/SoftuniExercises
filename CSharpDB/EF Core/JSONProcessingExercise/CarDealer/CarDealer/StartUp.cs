13a268097ac92e5d76cf4a163492cb9911e633b0
Velyan Gedzhev <v.gedzhev@gmail.com>
2021-03-14 19:17:00 +0200

    Add [Task 9. Import Suppliers] from JSONProcessingExercise

--- CSharpDB/EF Core/JSONProcessingExercise/CarDealer/CarDealer/StartUp.cs
+++ CSharpDB/EF Core/JSONProcessingExercise/CarDealer/CarDealer/StartUp.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Linq;
 using AutoMapper;
 using CarDealer.Data;
+using CarDealer.DTO;
 using CarDealer.Models;
 using Newtonsoft.Json;
 
@@ -13,6 +14,28 @@ namespace CarDealer
     {
         public static void Main(string[] args)
         {
+            var db = new CarDealerContext();
+            db.Database.EnsureDeleted();
+            db.Database.EnsureCreated();
+
+            var json = File.ReadAllText("../../../Datasets/suppliers.json");
+            Console.WriteLine(ImportSuppliers(db, json));
+        }
+
+        public static string ImportSuppliers(CarDealerContext context, string inputJson)
+        {
+            var suppliersDto = JsonConvert.DeserializeObject<IEnumerable<ImportSupplierInputModel>>(inputJson);
+
+            var suppliers = suppliersDto.Select(x => new Supplier
+            {
+                Name = x.Name,
+                IsImporter = x.IsImporter,
+            }).ToList();
+
+            context.Suppliers.AddRange(suppliers);
+            context.SaveChanges();
+
+            return $"Successfully imported {suppliers.Count()}."; ;
         }
     }
 }
