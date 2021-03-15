b8773bd537728be6ffa9636911b42fbd6f7a9678
Velyan Gedzhev <v.gedzhev@gmail.com>
2021-03-15 20:51:09 +0200

    Add [XMLProcessingDemo]

---
+++ CSharpDB/EF Core/XMLProcessing/XMLProcessingDemo/Article.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace XMLProcessingDemo
+{
+    [XmlType("doc")]
+    public class Article
+    {
+        [XmlElement("abstract")]
+        public string Abstract { get; set; }
+
+        [XmlElement("title")]
+        public string Title { get; set; }
+    }
+}
---
+++ CSharpDB/EF Core/XMLProcessing/XMLProcessingDemo/StartUp.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+using System.Xml.Serialization;
+
+namespace XMLProcessingDemo
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.Unicode;
+            //CreateNewXmlFile();
+
+            //XDocument document = XDocument.Load("../../../bgWiki.xml");
+
+            //var articles = document.Root.Elements().Where(x => x.Element("abstract").Value.Contains("програмис") || x.Element("abstract").Value.Contains("програмир"));
+
+            //foreach (var article in articles)
+            //{
+            //    Console.WriteLine(article.Element("title").Value
+            //        .Replace("Уикипедия: ", string.Empty));
+            //}
+
+
+            var serializer = new XmlSerializer(typeof(Article[]), new XmlRootAttribute("feed"));
+            IEnumerable<Article> articles = (Article[])serializer.Deserialize(File.OpenRead("../../../bgWiki.xml"));
+
+
+            foreach (var article in articles.Where(x => x.Abstract.Contains("програмис")
+            || x.Abstract.Contains("програмир")))
+            {
+                Console.WriteLine(article.Title.Replace("Уикипедия: ", string.Empty));
+            }
+        }
+
+        private static void CreateNewXmlFile()
+        {
+            XDocument document = new XDocument();
+            var root = new XElement("library");
+            document.Add(root);
+
+            for (int i = 0; i < 100; i++)
+            {
+                var book = new XElement("book");
+                root.Add(book);
+                book.Add(new XElement("title", i.ToString()));
+                book.Add(new XElement("price", i / 100.0));
+            }
+
+            document.Save("newTestXml.xml");
+        }
+    }
+}
