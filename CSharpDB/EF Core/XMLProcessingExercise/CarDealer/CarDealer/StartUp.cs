fe5b5b9b1eaae7a5ba50838a70f3cd5fb56b2f98
Velyan Gedzhev <v.gedzhev@gmail.com>
2021-03-16 19:16:01 +0200

    Add [Task 9. Import Suppliers] from XMLProcessingExercise

--- CSharpDB/EF Core/XMLProcessingExercise/CarDealer/CarDealer/StartUp.cs
+++ CSharpDB/EF Core/XMLProcessingExercise/CarDealer/CarDealer/StartUp.cs
@@ -1,10 +1,43 @@
-﻿namespace CarDealer
+﻿using CarDealer.Data;
+using CarDealer.DataTransferObjects.Input;
+using CarDealer.Models;
+using System;
+using System.IO;
+using System.Linq;
+using System.Xml.Serialization;
+
+namespace CarDealer
 {
     public class StartUp
     {
         public static void Main(string[] args)
         {
+            var db = new CarDealerContext();
+            db.Database.EnsureDeleted();
+            db.Database.EnsureCreated();
 
+            var supplierXml = File.ReadAllText("./Datasets/suppliers.xml");
+            var result = ImportSuppliers(db, supplierXml);
+            Console.WriteLine(result);
+        }
+
+        public static string ImportSuppliers(CarDealerContext context, string inputXml)
+        {
+            var xmlSerializer = new XmlSerializer(typeof(SupplierInputModel[]), new XmlRootAttribute("Suppliers"));
+            var textReader = new StringReader(inputXml);
+            var suppliersDto = (SupplierInputModel[])xmlSerializer.Deserialize(textReader);
+
+            var suppliers = suppliersDto
+                .Select(x => new Supplier
+                {
+                    Name = x.Name,
+                    IsImporter = x.IsImporter
+                }).ToList();
+
+            context.Suppliers.AddRange(suppliers);
+            context.SaveChanges();
+
+            return $"Successfully imported {suppliers.Count}";
         }
     }
 }
