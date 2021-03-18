4c42c711d56f924c2e3f3d647ca0dc552d54747e
Velyan Gedzhev <v.gedzhev@gmail.com>
2021-03-18 16:01:01 +0200

    Add [Task 2. Import Products] from XMLProcessingExercise

--- CSharpDB/EF Core/XMLProcessingExercise/CarDealer/CarDealer/DataTransferObjects/Output/SupplierOutputModel.cs
+++ CSharpDB/EF Core/XMLProcessingExercise/CarDealer/CarDealer/DataTransferObjects/Output/SupplierOutputModel.cs
@@ -5,7 +5,7 @@ using System.Xml.Serialization;
 
 namespace CarDealer.DataTransferObjects.Output
 {
-    [XmlType("supplier")]
+    [XmlType("suplier")]
     public class SupplierOutputModel
     {
         [XmlAttribute("id")]
--- CSharpDB/EF Core/XMLProcessingExercise/ProductShop/ProductShop/StartUp.cs
+++ CSharpDB/EF Core/XMLProcessingExercise/ProductShop/ProductShop/StartUp.cs
@@ -2,6 +2,7 @@
 using ProductShop.Dtos.Import;
 using ProductShop.Models;
 using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Xml.Serialization;
@@ -20,6 +21,37 @@ namespace ProductShop
             var result = ImportUsers(db, usersXml);
             Console.WriteLine(result);
 
+            var productsXml = File.ReadAllText("./Datasets/products.xml");
+            result = ImportProducts(db, productsXml);
+            Console.WriteLine(result);
+
+        }
+        public static string ImportProducts(ProductShopContext context, string inputXml)
+        {
+            var xmlSerializer = new XmlSerializer(typeof(ProductInputModel[]), new XmlRootAttribute("Products"));
+
+            var textReader = new StringReader(inputXml);
+
+            var productsDto = (ProductInputModel[])xmlSerializer.Deserialize(textReader);
+
+            var products = new List<Product>();
+
+            foreach (var productDto in productsDto)
+            {
+                var product = new Product()
+                {
+                    Name = productDto.Name,
+                    Price = productDto.Price,
+                    SellerId = productDto.SellerId,
+                    BuyerId = productDto.BuyerId,
+                };
+                products.Add(product);
+            }
+
+            context.Products.AddRange(products);
+            context.SaveChanges();
+
+            return $"Successfully imported {products.Count}";
         }
 
         public static string ImportUsers(ProductShopContext context, string inputXml)
