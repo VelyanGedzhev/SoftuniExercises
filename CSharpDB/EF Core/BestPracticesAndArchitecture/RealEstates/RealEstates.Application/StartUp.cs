84b1e246f4ef99be492d17834a3d15fd20f61077
Velyan Gedzhev <v.gedzhev@gmail.com>
2021-03-19 11:34:01 +0200

    Add architecture for BestPracticesAndArchitecture

---
+++ CSharpDB/EF Core/BestPracticesAndArchitecture/RealEstates/RealEstates.Application/StartUp.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RealEstates.Data;
+using System;
+
+namespace RealEstates.Application
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var db = new ApplicationDbContext();
+            db.Database.Migrate();
+        }
+    }
+}
---
+++ CSharpDB/EF Core/BestPracticesAndArchitecture/RealEstates/RealEstates.Data/ApplicationDbContext.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RealEstates.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext()
+        {
+
+        }
+
+        public ApplicationDbContext(DbContextOptions options)
+            :base(options)
+        {
+
+        }
+
+
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Server=.;Database=RealEstate;Integrated Security=true;");
+            }
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
