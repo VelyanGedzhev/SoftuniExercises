dae59217b845538e4ca4b87f8f41dc84c47b3ae5
Velyan Gedzhev <v.gedzhev@gmail.com>
2021-03-23 12:31:06 +0200

    Add models

--- CSharpDB/EF Core/QuizWorkshop/Quiz.Data/ApplicationDbContext.cs
+++ CSharpDB/EF Core/QuizWorkshop/Quiz.Data/ApplicationDbContext.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Quiz.Models;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -13,5 +14,9 @@ namespace Quiz.Data
             : base(options)
         {
         }
+
+        public DbSet<Models.Quiz> Quizzes { get; set; }
+        public DbSet<Question> Questions { get; set; }
+        public DbSet<Answer> Answers { get; set; }
     }
 }
