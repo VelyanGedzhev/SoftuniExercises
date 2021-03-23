d7125b3fd120ac396831c6f12896e1745b3cd459
Velyan Gedzhev <v.gedzhev@gmail.com>
2021-03-23 12:36:13 +0200

    Add [UserAnswer] model

---
+++ CSharpDB/EF Core/QuizWorkshop/Quiz.Models/UserAnswer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quiz.Models
+{
+    public class UserAnswer
+    {
+        public int UserId { get; set; }
+
+        public int QiuzId { get; set; }
+
+        public int QuestionId { get; set; }
+
+        public int AnswerId { get; set; }
+    }
+}
