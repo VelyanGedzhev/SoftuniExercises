0c413c8976fda72282a89b66648696b4ab4be3ab
Velyan Gedzhev <v.gedzhev@gmail.com>
2021-03-29 17:14:53 +0300

    Add [Task 2. Data Import] from [RetakeExam14Aug2020]

---
+++ CSharpDB/EF Core/ExamPreparation/RetakeExam14Aug2020/SoftJail/DataProcessor/ImportDto/OfficerPrisonerInputModel.cs
@@ -0,0 +1,37 @@
+﻿using SoftJail.Data.Models.Enums;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace SoftJail.DataProcessor.ImportDto
+{
+    [XmlType("Officer")]
+    public class OfficerPrisonerInputModel
+    {
+        [XmlElement("Name")]
+        [Required]
+        [StringLength(30, MinimumLength =3)]
+        public string Name { get; set; }
+
+        [XmlElement("Money")]
+        [Range(typeof(decimal), "0", "79228162514264337593543950335")]
+        public decimal Money { get; set; }
+
+
+        [XmlElement("Position")]
+        [EnumDataType(typeof(Position))]
+        public string Position { get; set; }
+
+        [XmlElement("Weapon")]
+        [EnumDataType(typeof(Weapon))]
+        public string Weapon { get; set; }
+
+        [XmlElement("DepartmentId")]
+        public int DepartmentId { get; set; }
+
+        [XmlArray("Prisoners")]
+        public PrisonerInputModel[] Prisoners { get; set; }
+    }
+}
