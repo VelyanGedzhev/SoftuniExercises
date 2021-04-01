32cf9bda1c246645344f02a316d6f6a93713e748
Velyan Gedzhev <v.gedzhev@gmail.com>
2021-04-01 13:21:22 +0300

    Add [Task 3. Data Export] from [Exam13Dec2019]

--- CSharpDB/EF Core/ExamPreparation/Exam13Dec2019/BookShop/DataProcessor/Serializer.cs
+++ CSharpDB/EF Core/ExamPreparation/Exam13Dec2019/BookShop/DataProcessor/Serializer.cs
@@ -7,20 +7,64 @@
     using System.Text;
     using System.Xml;
     using System.Xml.Serialization;
+    using BookShop.DataProcessor.ExportDto;
     using Data;
     using Newtonsoft.Json;
     using Formatting = Newtonsoft.Json.Formatting;
+    using Data.Models;
 
     public class Serializer
     {
         public static string ExportMostCraziestAuthors(BookShopContext context)
         {
-            return "TODO";
+            var authors = context.Authors.ToList()
+                .Select(a => new
+                {
+                    AuthorName = a.FirstName + " " + a.LastName,
+                    Books = a.AuthorsBooks
+                    .OrderByDescending(bp => bp.Book.Price)
+                    .Select(b => new
+                    {
+                        BookName = b.Book.Name,
+                        BookPrice = b.Book.Price.ToString("F2"),
+                    })
+                    .ToList()
+                })
+                .ToList()
+                .OrderByDescending(x => x.Books.Count())
+                .ThenBy(x => x.AuthorName)
+                .ToList();
+                
+
+            return JsonConvert.SerializeObject(authors, Formatting.Indented);
         }
 
         public static string ExportOldestBooks(BookShopContext context, DateTime date)
         {
-            return "TODO";
+            var books = context.Books
+                .Where(x => x.PublishedOn < date && x.Genre == Genre.Science)
+                .ToArray()
+                .OrderByDescending(x => x.Pages)
+                .ThenByDescending(x => x.PublishedOn)
+                .Take(10)
+                .Select(b => new BookXmlOutputModel
+                {
+                    Name = b.Name,
+                    Pages = b.Pages,
+                    Date = b.PublishedOn.ToString("d",CultureInfo.InvariantCulture),
+                })
+                .ToArray();
+
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(BookXmlOutputModel[]), new XmlRootAttribute("Books"));
+
+            XmlSerializerNamespaces ns = new XmlSerializerNamespaces();
+            ns.Add(string.Empty, string.Empty);
+            var sb = new StringBuilder();
+            var stringWriter = new StringWriter(sb);
+
+            xmlSerializer.Serialize(stringWriter, books, ns);
+
+            return sb.ToString().TrimEnd();
         }
     }
 }
