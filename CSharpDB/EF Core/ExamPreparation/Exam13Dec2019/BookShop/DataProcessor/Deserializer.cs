0dc493943de4eb41b05599aed77b0f63895f8bf2
Velyan Gedzhev <v.gedzhev@gmail.com>
2021-04-01 12:33:06 +0300

    Add [Task 2. Data Import] from [Exam13Dec2019]

--- CSharpDB/EF Core/ExamPreparation/Exam13Dec2019/BookShop/DataProcessor/Deserializer.cs
+++ CSharpDB/EF Core/ExamPreparation/Exam13Dec2019/BookShop/DataProcessor/Deserializer.cs
@@ -8,6 +8,8 @@
     using System.Linq;
     using System.Text;
     using System.Xml.Serialization;
+    using BookShop.Data.Models;
+    using BookShop.DataProcessor.ImportDto;
     using Data;
     using Newtonsoft.Json;
     using ValidationContext = System.ComponentModel.DataAnnotations.ValidationContext;
@@ -24,12 +26,114 @@
 
         public static string ImportBooks(BookShopContext context, string xmlString)
         {
-           throw new NotImplementedException();
+            var output = new StringBuilder();
+            var books = new List<Book>();
+
+            var xmlSerlializer =
+                new XmlSerializer(typeof(BookInputModelXml[]), new XmlRootAttribute("Books"));
+
+            var xmlBooks = (BookInputModelXml[])xmlSerlializer.Deserialize(new StringReader(xmlString));
+
+            foreach (var xmlBook in xmlBooks)
+            {
+                if (!IsValid(xmlBook))
+                {
+                    output.AppendLine(ErrorMessage);
+                    continue;
+                }
+
+                var isValidDate =
+                    DateTime.TryParseExact(xmlBook.PublishedOn, "MM/dd/yyyy",
+                    CultureInfo.InvariantCulture,
+                    DateTimeStyles.None,
+                    out DateTime date);
+
+                if (!isValidDate)
+                {
+                    output.AppendLine(ErrorMessage);
+                    continue;
+                }
+
+                var book = new Book
+                {
+                    Name = xmlBook.Name,
+                    Genre = Enum.Parse<Genre>(xmlBook.Genre),
+                    Price = xmlBook.Price,
+                    Pages = xmlBook.Pages,
+                    PublishedOn = date,
+                };
+
+                output.AppendLine(string.Format(SuccessfullyImportedBook, book.Name, book.Price));
+
+                books.Add(book);
+            }
+
+            context.Books.AddRange(books);
+            context.SaveChanges();
+
+            return output.ToString().TrimEnd();
+
         }
 
         public static string ImportAuthors(BookShopContext context, string jsonString)
         {
-            throw new NotImplementedException();
+            var output = new StringBuilder();
+            var authors = new List<Author>();
+
+            var jsonAuthours = JsonConvert.DeserializeObject<IEnumerable<AuthorInputModelJson>>(jsonString);
+
+            foreach (var jsonAuthor in jsonAuthours)
+            {
+                if (!IsValid(jsonAuthor) ||
+                    !jsonAuthor.Books.Any() ||
+                    authors.Any(x => x.Email == jsonAuthor.Email))
+                {
+                    output.AppendLine(ErrorMessage);
+                    continue;
+                }
+
+                var author = new Author
+                {
+                    FirstName = jsonAuthor.FirstName,
+                    LastName = jsonAuthor.LastName,
+                    Phone = jsonAuthor.Phone,
+                    Email = jsonAuthor.Email,
+                };
+
+                foreach (var jsonBook in jsonAuthor.Books)
+                {
+                    if (!jsonBook.Id.HasValue ||
+                        !context.Books.Any(x => x.Id == jsonBook.Id))
+                    {
+                        continue;
+                    }
+
+                    var book = context.Books
+                        .FirstOrDefault(x => x.Id == jsonBook.Id);
+
+                    author.AuthorsBooks.Add(new AuthorBook
+                    {
+                        Author = author,
+                        Book = book,
+                    });
+                }
+
+                if (!author.AuthorsBooks.Any())
+                {
+                    output.AppendLine(ErrorMessage);
+                    continue;
+                }
+
+                authors.Add(author);
+                output.AppendLine(string.Format(
+                    SuccessfullyImportedAuthor, author.FirstName + 
+                    " " + author.LastName, author.AuthorsBooks.Count));
+
+            }
+            context.AttachRange(authors);
+            context.SaveChanges();
+
+            return output.ToString().TrimEnd();
         }
 
         private static bool IsValid(object dto)
