3b041a0f65e6dfe6a8bfec560dc05198196bf89d
Velyan Gedzhev <v.gedzhev@gmail.com>
2021-04-14 13:51:05 +0300

    Add [Task 3. Implement ADS IAbstractQueue<T>]

--- DataStructuresFundamentals/LinearDataStructures/Lab/Problem03.Queue/Queue.cs
+++ DataStructuresFundamentals/LinearDataStructures/Lab/Problem03.Queue/Queue.cs
@@ -8,34 +8,95 @@
     {
         private Node<T> _head;
 
+        public Queue()
+        {
+            this._head = null;
+            Count = 0;
+        }
+
+        public Queue(Node<T> head)
+        {
+            this._head = head;
+            Count = 1;
+        }
+
         public int Count { get; private set; }
 
         public bool Contains(T item)
         {
-            throw new NotImplementedException();
+            var current = this._head;
+
+            while (current != null)
+            {
+                if (current.Item.Equals(item))
+                {
+                    return true;
+                }
+                current = current.Next;
+            }
+
+            return false;
         }
 
         public T Dequeue()
         {
-            throw new NotImplementedException();
+            ValidateIfNotEmpty();
+
+            var current = this._head;
+            this._head = this._head.Next;
+            Count--;
+            return current.Item;
         }
 
         public void Enqueue(T item)
         {
-            throw new NotImplementedException();
+            var current = this._head;
+            var elementToInsert = new Node<T>(item);
+
+            if (current == null)
+            {
+                this._head = elementToInsert;
+            }
+            else
+            {
+                while (current.Next != null)
+                {
+                    current = current.Next;
+                }
+
+                current.Next = elementToInsert;
+            }
+
+            Count++;
         }
+
 
         public T Peek()
         {
-            throw new NotImplementedException();
+            ValidateIfNotEmpty();
+            return this._head.Item;
         }
 
         public IEnumerator<T> GetEnumerator()
         {
-            throw new NotImplementedException();
+            var current = this._head;
+
+            while (current != null)
+            {
+                yield return current.Item;
+                current = current.Next;
+            }
         }
 
         IEnumerator IEnumerable.GetEnumerator()
-            => throw new NotImplementedException();
+            => GetEnumerator();
+
+        private void ValidateIfNotEmpty()
+        {
+            if (Count == 0)
+            {
+                throw new InvalidOperationException();
+            }
+        }
     }
 }
