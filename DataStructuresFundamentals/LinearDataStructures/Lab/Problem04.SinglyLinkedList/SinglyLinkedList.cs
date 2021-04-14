6ac5d94f66af9ff14e706f36667097bd8b96c8de
Velyan Gedzhev <v.gedzhev@gmail.com>
2021-04-14 14:45:16 +0300

    Add [Task 4. Implement ADS IAbstractLinkedList<T>]

--- DataStructuresFundamentals/LinearDataStructures/Lab/Problem04.SinglyLinkedList/SinglyLinkedList.cs
+++ DataStructuresFundamentals/LinearDataStructures/Lab/Problem04.SinglyLinkedList/SinglyLinkedList.cs
@@ -8,44 +8,119 @@
     {
         private Node<T> _head;
 
+        public SinglyLinkedList()
+        {
+            this._head = null;
+            Count = 0;
+        }
+
+        public SinglyLinkedList(Node<T> topElement)
+        {
+            this._head = topElement;
+            Count = 1;
+        }
+
         public int Count { get; private set; }
 
         public void AddFirst(T item)
         {
-            throw new NotImplementedException();
+            var itemToInsert = new Node<T>(item, this._head);
+            this._head = itemToInsert;
+            Count++;
         }
 
         public void AddLast(T item)
         {
-            throw new NotImplementedException();
+            var itemToInsert = new Node<T>(item);
+            var current = this._head;
+
+            if (current == null)
+            {
+                this._head = itemToInsert;
+                Count++;
+            }
+
+            else
+            {
+                while (current.Next != null)
+                {
+                    current = current.Next;
+                }
+
+                current.Next = itemToInsert;
+                Count++;
+            }
         }
 
         public T GetFirst()
         {
-            throw new NotImplementedException();
+            ValidateIfIsEmpty();
+
+            return this._head.Item;
         }
 
         public T GetLast()
         {
-            throw new NotImplementedException();
+            ValidateIfIsEmpty();
+
+            var itemToReturn = this._head;
+
+            while (itemToReturn.Next != null)
+            {
+                itemToReturn = itemToReturn.Next;
+            }
+
+            return itemToReturn.Item;
         }
 
         public T RemoveFirst()
         {
-            throw new NotImplementedException();
+            ValidateIfIsEmpty();
+            var itemToReturn = this._head;
+            this._head = this._head.Next;
+
+            Count--;
+
+            return itemToReturn.Item;
         }
 
         public T RemoveLast()
         {
-            throw new NotImplementedException();
+            ValidateIfIsEmpty();
+            var itemToRemove = this._head;
+            var currentLast = this._head;
+
+            while (itemToRemove.Next != null)
+            {
+                currentLast = itemToRemove;
+                itemToRemove = itemToRemove.Next;
+            }
+            currentLast.Next = null;
+            Count--;
+
+            return itemToRemove.Item;
         }
 
         public IEnumerator<T> GetEnumerator()
         {
-            throw new NotImplementedException();
+            var current = this._head;
+
+            while (current != null)
+            {
+                yield return current.Item;
+                current = current.Next;
+            }
         }
 
         IEnumerator IEnumerable.GetEnumerator()
             => this.GetEnumerator();
+
+        private void ValidateIfIsEmpty()
+        {
+            if (Count == 0)
+            {
+                throw new InvalidOperationException();
+            }
+        }
     }
 }
--- DataStructuresFundamentals/LinearDataStructures/Lab/SinglyLinkedList.Tests/SinglyLinkedListTests.cs
+++ DataStructuresFundamentals/LinearDataStructures/Lab/SinglyLinkedList.Tests/SinglyLinkedListTests.cs
@@ -38,7 +38,7 @@ namespace SinglyLinkedList.Tests
         [Test]
         public void AddLastShouldWorkAsExpected()
         {
-            var count = this._random.Next(10, 30);
+            var count = this._random.Next(4, 6);
             var array = new int[count];
 
             for (var i = 0; i < count; i++)
