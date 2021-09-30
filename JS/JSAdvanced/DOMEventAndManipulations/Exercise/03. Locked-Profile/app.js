0de472d7feea09604d853e9e69d455d62765ac45
Velyan Gedzhev <v.gedzhev@gmail.com>
2021-09-30 18:41:20 +0300

    Add Task 6. Furniture

--- JS/JSAdvanced/DOMEventAndManipulations/Exercise/03. Locked-Profile/app.js
+++ JS/JSAdvanced/DOMEventAndManipulations/Exercise/03. Locked-Profile/app.js
@@ -1,3 +1,23 @@
 function lockedProfile() {
-    console.log('TODO...')
+    Array
+        .from(document.querySelectorAll('.profile button'))
+        .forEach(b => b.addEventListener('click', onToggle));
+
+    function onToggle(e) {
+        const profile = e.target.parentElement;
+        const isUnlocked = profile.querySelector('input[type="radio"][value="unlock"]').checked;
+
+
+        if (isUnlocked) {
+            const infoDiv = profile.querySelector('div');
+
+            if (e.target.textContent == 'Show more') {
+                infoDiv.style.display = 'block';
+                e.target.textContent = 'Hide it';
+            } else {
+                infoDiv.style.display = '';
+                e.target.textContent = 'Show more';
+            }
+        }
+    }
 }
--- JS/JSAdvanced/DOMEventAndManipulations/Exercise/06. Furniture/solution.js
+++ JS/JSAdvanced/DOMEventAndManipulations/Exercise/06. Furniture/solution.js
@@ -1,4 +1,69 @@
 function solve() {
+  const table = document.querySelector('table.table tbody');
+  const [input, output] = Array.from(document.querySelectorAll('textarea'));
+  const [generateBtn, buyBtn] = Array.from(document.querySelectorAll('button'));
 
-  //TODO...
+  generateBtn.addEventListener('click', generate);
+  buyBtn.addEventListener('click', buy);
+
+  function createCell(nestedTag, properties, content) {
+    const cell = document.createElement('td');
+    const nestedElement = document.createElement(nestedTag);
+
+    for (let prop in properties) {
+      nestedElement = properties[prop];
+    }
+
+    if (content) {
+      nestedElement.textContent = content;
+    }
+
+    cell.appendChild(nestedElement);
+    return cell;
+  }
+
+  function generate(e) {
+    const data = JSON.parse(input.value);
+
+    for (let item of data) {
+      const row = document.createElement('tr');
+
+      row.appendChild(createCell('img', { src: item.img }));
+      row.appendChild(createCell('p', {}, item.name));
+      row.appendChild(createCell('p', {}, item.price));
+      row.appendChild(createCell('p', {}, item.decFactor));
+      row.appendChild(createCell('input', { type: 'checkbox' }));
+
+      table.appendChild(row);
+    }
+  }
+
+  function buy(e) {
+    const furniture = Array
+      .from(document.querySelectorAll('input[type=checkbox]:checked'))
+      .map(b => b.parentElement.parentElement)
+      .map(r => ({
+        name: r.children[1].textContent,
+        price: Number(r.children[2].textContent),
+        decFactor: Number(r.children[3].textContent)
+      }));
+
+    const names = [];
+    let total = 0;
+    let decFactor = 0;
+
+    for (let item of furniture) {
+      names.push(item.name);
+      total += item.price;
+      decFactor += item.decFactor;
+    }
+
+    decFactor = decFactor / furniture.length;
+
+    const result = `Bought furniture: ${names.join(', ')}
+Total price: ${total.toFixed(2)}
+Average decoration factor: ${decFactor}`;
+
+    output.textContent = result;
+  }
 }
