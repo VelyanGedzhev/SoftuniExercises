08d16e0383406af6db110655bcc1fac76d94bc9d
Velyan Gedzhev <v.gedzhev@gmail.com>
2022-03-31 16:25:14 +0300

    Add core and themes modules

---
+++ FrontEndModule/Angular/Workshops/componentsNew/src/app/core/core.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { UserService } from './user.service';
+import { storageServiceProvider } from './storage.service';
+import { PostService } from './post.service';
+import { ThemeService } from './theme.service';
+
+
+
+@NgModule({
+  declarations: [
+    HeaderComponent,
+    FooterComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    HeaderComponent,
+    FooterComponent
+  ],
+  providers: [
+    UserService,
+    ThemeService,
+    storageServiceProvider,
+    PostService
+  ]
+})
+export class CoreModule { }
