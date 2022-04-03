39f0a9a092e8c0145cd01bcc75841ff7ad0b8776
Velyan Gedzhev <v.gedzhev@gmail.com>
2022-04-03 15:40:47 +0300

    Add custom validation for emails

--- FrontEndModule/Angular/Workshops/src/app/auth/login/login.component.ts
+++ FrontEndModule/Angular/Workshops/src/app/auth/login/login.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { UserService } from 'src/app/core/user.service';
+import { customEmailValidator } from './util';
 
 @Component({
   selector: 'app-login',
@@ -13,7 +14,8 @@ export class LoginComponent implements OnInit {
   loginFormGroup: FormGroup = this.formBuilder.group({
     'email': new FormControl(null, [
         Validators.required,
-        Validators.email
+        customEmailValidator
+        // Validators.email
       ]),
     "password": new FormControl(null, [
         Validators.required,
